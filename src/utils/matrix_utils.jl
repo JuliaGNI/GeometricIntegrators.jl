74282ed928a9991cd5dbb169885976d3a0b70a1b
Michael Kraus <michael.kraus@ipp.mpg.de>
2016-09-11 09:18:37 +0200

    Added new matrix functions, e.g., for matrix vector multiplication and vector addition.

--- src/utils/matrix_utils.jl
+++ src/utils/matrix_utils.jl
@@ -18,3 +18,44 @@ function istrilstrict(A::AbstractMatrix)
     end
     return true
 end
+
+function simd_scale!(x, a)
+    @simd for i=1:length(x)
+        @inbounds x[i] *= a
+    end
+end
+
+"Copy the first dimension of a 2D array y into a 1D array x."
+function simd_copy_xy_first!(x, y, j)
+    @simd for i=1:length(x)
+        @inbounds x[i] = y[i,j]
+    end
+end
+
+"Copy a 1D array x into the first dimension of a 2D array y."
+function simd_copy_yx_first!(x, y, j)
+    @simd for i=1:length(x)
+        @inbounds y[i,j] = x[i]
+    end
+end
+
+function simd_axpy!(a, x, y)
+    @simd for i=1:length(x)
+        @inbounds y[i] += a*x[i]
+    end
+end
+
+function simd_waxpy!(w, a, x, y)
+    @simd for i=1:length(x)
+        @inbounds w[i] = a*x[i] + y[i]
+    end
+end
+
+function simd_mult!(w, X, y)
+    @simd for i=1:length(w)
+        w[i] = 0
+        @simd for j=1:length(y)
+            @inbounds w[i] += X[i,j] * y[j]
+        end
+    end
+end
