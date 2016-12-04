fb7194aff80a090605f681bfc3661998b281f0de
Michael Kraus <michael.kraus@ipp.mpg.de>
2016-12-04 21:34:20 +0900

    Partial implementation of implicit partitioned Runge-Kutta methods for special ODEs.

--- src/integrators/integrators_iprk.jl
+++ src/integrators/integrators_iprk.jl
@@ -1,14 +1,25 @@
 
-"Parameters for right-hand side function of special partitioned Runge-Kutta methods."
-immutable NonlinearFunctionParametersIPRK{T} <: NonlinearFunctionParameters{T}
-    f::Function
-    g::Function
+using GeomDAE.Fields: Field, Q, P, V, F
+
+"Parameters for right-hand side function of implicit partitioned Runge-Kutta methods."
+type NonlinearFunctionParametersIPRK{T} <: NonlinearFunctionParameters{T}
+    fields::Tuple{Vararg{Field}}
+    functs::Tuple{Vararg{Field}}
+
+    fq::Function
+    fp::Function
+    fv::Function
+    ff::Function
+
     Δt::T
+    t::T
 
     d::Int
     s::Int
     a_q::Matrix{T}
     a_p::Matrix{T}
+    c_q::Vector{T}
+    c_p::Vector{T}
 
     q::Vector{T}
     p::Vector{T}
@@ -19,15 +30,15 @@ immutable NonlinearFunctionParametersIPRK{T} <: NonlinearFunctionParameters{T}
     V::Matrix{T}
     P::Matrix{T}
     F::Matrix{T}
-    Y::Matrix{T}
-    Z::Matrix{T}
+
+    Y::Array{T,3}
 
     tQ::Vector{T}
     tV::Vector{T}
     tP::Vector{T}
     tF::Vector{T}
 
-    function NonlinearFunctionParametersIPRK(f, g, Δt, d, s, a_q, a_p)
+    function NonlinearFunctionParametersIPRK(fields, functs, fq, fp, fv, ff, Δt, d, s, a_q, a_p, c_q, c_p)
         # create solution vectors
         q = zeros(T,d)
         p = zeros(T,d)
@@ -39,58 +50,95 @@ immutable NonlinearFunctionParametersIPRK{T} <: NonlinearFunctionParameters{T}
         V = zeros(T,d,s)
         P = zeros(T,d,s)
         F = zeros(T,d,s)
-        Y = zeros(T,d,s)
-        Z = zeros(T,d,s)
 
         # create temporary vectors
+        Y  = zeros(T,2,d,s)
+
         tQ = zeros(T,d)
         tV = zeros(T,d)
         tP = zeros(T,d)
         tF = zeros(T,d)
 
-        new(f, g, Δt, d, s, a_q, a_p, q, p, y, z, Q, V, P, F, Y, Z, tQ, tV, tP, tF)
+        new(fields, functs, fq, fp, fv, ff, Δt, 0, d, s, a_q, a_p, c_q, c_p, q, p, y, z, Q, V, P, F, Y, tQ, tV, tP, tF)
     end
 end
 
-"Compute stages of special partitioned Runge-Kutta methods."
-function function_stages!{T}(y::Vector{T}, b::Vector{T}, params::NonlinearFunctionParametersIPRK{T})
-    for i in 1:params.s
-        for k in 1:params.d
-            # copy y to Y and Z
-            params.Y[k,i] = y[2*(params.d*(i-1)+k-1)+1]
-            params.Z[k,i] = y[2*(params.d*(i-1)+k-1)+2]
+"Compute stages of implicit partitioned Runge-Kutta methods."
+@generated function function_stages!{T}(y::Vector{T}, b::Vector{T},
+                             params::NonlinearFunctionParametersIPRK{T},
+                             fields::Field...)
 
-            # compute Q and V
-            params.Q[k,i] = params.q[k] + params.Δt * params.Y[k,i]
-            params.V[k,i] = params.Z[k,i]
-        end
+    local fargs = []
 
-        # compute f(X)
-        simd_copy_xy_first!(params.tQ, params.Q, i)
-        simd_copy_xy_first!(params.tV, params.V, i)
-        params.f(params.tQ, params.tV, params.tP)
-        params.g(params.tQ, params.tV, params.tF)
-        simd_copy_yx_first!(params.tP, params.P, i)
-        simd_copy_yx_first!(params.tF, params.F, i)
+     if Q ∈ fields
+         push!(fargs, :(params.tQ))
+     end
+
+     if V ∈ fields
+         push!(fargs, :(params.tV))
+     end
+
+     if P ∈ fields
+         push!(fargs, :(params.tP))
+     end
+
+     if F ∈ fields
+         push!(fargs, :(params.tF))
+     end
+
+     code = quote
+         local tqᵢ::T
+         local tpᵢ::T
+
+         # loop through stages
+         for i in 1:params.s
+             for k in 1:params.d
+                 # copy y to Y
+                 params.Y[1,k,i] = y[2*(params.d*(i-1)+k-1)+1]
+                 params.Y[2,k,i] = y[2*(params.d*(i-1)+k-1)+2]
+
+                 # compute Q and V
+                 params.Q[k,i] = params.q[k] + params.Δt * params.Y[1,k,i]
+                 params.V[k,i] = params.Y[2,k,i]
+             end
+
+             # compute f(X)
+             tqᵢ = params.t + params.Δt * params.c_q[i]
+             tpᵢ = params.t + params.Δt * params.c_p[i]
+             simd_copy_xy_first!(params.tQ, params.Q, i)
+             simd_copy_xy_first!(params.tV, params.V, i)
+             params.fp(tpᵢ, params.tQ, params.tV, params.tP)
+             params.ff(tpᵢ, params.tQ, params.tV, params.tF)
+             simd_copy_yx_first!(params.tP, params.P, i)
+             simd_copy_yx_first!(params.tF, params.F, i)
+         end
+
+         # compute b = - [(Y-AV), (P-AF)]
+         for i in 1:params.s
+             for k in 1:params.d
+                 b[2*(params.d*(i-1)+k-1)+1] = - params.Y[1,k,i]
+                 b[2*(params.d*(i-1)+k-1)+2] = - params.P[k,i] + params.p[k]
+                 for j in 1:params.s
+                     b[2*(params.d*(i-1)+k-1)+1] += params.a_q[i,j] * params.V[k,j]
+                     b[2*(params.d*(i-1)+k-1)+2] += params.a_p[i,j] * params.F[k,j] * params.Δt
+                 end
+             end
+         end
     end
 
-    # compute b = - [(Y-AV), (P-AF)]
-    for i in 1:params.s
-        for k in 1:params.d
-            b[2*(params.d*(i-1)+k-1)+1] = - params.Y[k,i]
-            b[2*(params.d*(i-1)+k-1)+2] = - params.P[k,i] + params.p[k]
-            for j in 1:params.s
-                b[2*(params.d*(i-1)+k-1)+1] += params.a_q[i,j] * params.V[k,j]
-                b[2*(params.d*(i-1)+k-1)+2] += params.a_p[i,j] * params.F[k,j] * params.Δt
-            end
-        end
-    end
+    return code
+end
+
+"Compute stages of implicit partitioned Runge-Kutta methods."
+function function_stages!{T}(y::Vector{T}, b::Vector{T},
+                             params::NonlinearFunctionParametersIPRK{T})
+    function_stages!(y, b, params, params.fields...)
 end
 
 
-"Special partitioned Runge-Kutta integrator."
+"Implicit partitioned Runge-Kutta integrator."
 immutable IntegratorIPRK{T, ST} <: Integrator{T}
-    equation::SPODE{T}
+    equation::SODE{T}
     tableau::TableauIPRK{T}
     Δt::T
 
@@ -104,11 +152,9 @@ immutable IntegratorIPRK{T, ST} <: Integrator{T}
     V::Array{T,2}
     P::Array{T,2}
     F::Array{T,2}
-    Y::Array{T,2}
-    Z::Array{T,2}
 end
 
-function IntegratorIPRK{T}(equation::SPODE{T}, tableau::TableauIPRK{T}, Δt::T)
+function IntegratorIPRK{T}(equation::SODE{T}, tableau::TableauIPRK{T}, Δt::T; nonlinear_solver=QuasiNewtonSolver)
     D = equation.d
     S = tableau.s
 
@@ -116,17 +162,18 @@ function IntegratorIPRK{T}(equation::SPODE{T}, tableau::TableauIPRK{T}, Δt::T)
     z = zeros(T, 2*D*S)
 
     # create params
-    params = NonlinearFunctionParametersIPRK{T}(equation.f, equation.g, Δt, D, S, tableau.a_q, tableau.a_p)
+    params = NonlinearFunctionParametersIPRK{T}(equation.fields, equation.functs,
+                                                equation.q, equation.p, equation.v, equation.f,
+                                                Δt, D, S,
+                                                tableau.a_q, tableau.a_p, tableau.c_q, tableau.c_p)
 
     # create solver
-    solver = QuasiNewtonSolver(z, params)
-    # TODO allow for other nonlinear solvers based on constructor argument
+    solver = nonlinear_solver(z, params)
 
     # create integrator
     IntegratorIPRK{T, typeof(solver)}(equation, tableau, Δt, solver,
                                       params.q, params.p, params.y, params.z,
-                                      params.Q, params.V, params.P, params.F,
-                                      params.Y, params.Z)
+                                      params.Q, params.V, params.P, params.F)
 end
 
 
@@ -143,7 +190,7 @@ function integrate!(int::IntegratorIPRK, sol::SolutionPODE)
         # TODO
         for i in 1:int.tableau.s
             for k in 1:int.equation.d
-                int.solver.x[2*(sol.d*(i-1)+k-1)+1] = int.q[k]
+                int.solver.x[2*(sol.nd*(i-1)+k-1)+1] = int.q[k]
                 # TODO initial guess for velocity
                 # int.solver.x[2*(params.d*(i-1)+k-1)+1] = 0.
             end
@@ -155,7 +202,7 @@ function integrate!(int::IntegratorIPRK, sol::SolutionPODE)
         if (int.solver.status.rₐ > int.solver.params.atol² &&
             int.solver.status.rᵣ > int.solver.params.rtol  &&
             int.solver.status.rₛ > int.solver.params.stol²)||
-            int.solver.status.i >= int.solver.params.nmax
+            int.solver.status.i  ≥ int.solver.params.nmax
             println(int.solver.status.i, ", ", int.solver.status.rₐ,", ",  int.solver.status.rᵣ,", ",  int.solver.status.rₛ)
         end
 
@@ -170,7 +217,6 @@ function integrate!(int::IntegratorIPRK, sol::SolutionPODE)
             nt = div(n, sol.nsave)+1
             simd_copy_yx_first!(int.q, sol.q, nt)
             simd_copy_yx_first!(int.p, sol.p, nt)
-            sol.t[nt] = sol.t[1] + n * int.Δt
         end
     end
     nothing
