3a3517268bbffdeabacdd51caa22c5e9b3b67871
Michael Kraus <michael.kraus@ipp.mpg.de>
2016-12-21 11:45:19 +0900

    Add rudimentary tests for some more integrators.

--- test/integrators/integrators_test.jl
+++ test/integrators/integrators_test.jl
@@ -7,10 +7,20 @@ nt = 10
 @test typeof(Integrator(ODE(fx, [1.]), getTableauImplicitMidpoint(), Δt)) <: IntegratorFIRK
 
 ode = ODE(fx, [1.])
+pode = PODE(fx, fx, [1.], [1.])
+iode = IODE(fq, fp, [1.], [1.])
+
 int = Integrator(ode, getTableauERK4(), Δt)
 sol = integrate(int, nt)
 
+int = Integrator(ode, getTableauImplicitMidpoint(), Δt)
+sol = integrate(int, nt)
 
-pode = PODE(fq, fp, [1.], [1.])
 pint = Integrator(pode, getTableauSymplecticEulerA(), Δt)
-psol = integrate(int, nt)
+psol = integrate(pint, nt)
+
+iint = Integrator(iode, TableauIPRK(:pglrk, 2, getTableauGLRK1(), getTableauGLRK1()), Δt)
+isol = integrate(iint, nt)
+
+vint = Integrator(iode, getTableauLobIIIAB2(), Δt)
+vsol = integrate(vint, nt)
