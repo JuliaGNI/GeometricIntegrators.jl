9a5c90bf86c36999468c07854fffda8bc1060203
Michael Kraus <michael.kraus@ipp.mpg.de>
2016-12-21 11:34:45 +0900

    Fixes in documentation.

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,9 @@ makedocs(
              "Tutorial" => "tutorial.md",
              "Modules"  => [
                 "Equations"         => "modules/equations.md",
+                "Solutions"         => "modules/solutions.md",
                 "Integrators"       => "modules/integrators.md",
+                "Interpolation"     => "modules/interpolation.md",
                 "Linear Solvers"    => "modules/solvers_linear.md",
                 "Nonlinear Solvers" => "modules/solvers_nonlinear.md",
                 "Tableaus"          => "modules/tableaus.md"]
