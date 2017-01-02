2e75de3acb70ef865d0d99437dbfbefc234ab87c
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-01-02 16:49:13 +0900

    Added Lotka-Volterra 2D example problem.

--- src/Problems.jl
+++ src/Problems.jl
@@ -4,6 +4,10 @@ module Problems
 
     using ..Equations
 
+    export lotka_volterra_2d_ode, lotka_volterra_2d_iode, lotka_volterra_2d_idae
+
+    include("problems/lotka_volterra_2d.jl")
+
     export pendulum_ode, pendulum_pode, pendulum_iode, pendulum_idae
 
     include("problems/pendulum.jl")
---
+++ src/problems/lotka_volterra_2d.jl
@@ -0,0 +1,53 @@
+
+function lotka_volterra_2d_ode_f_params(t, x, f, a1, b1, a2, b2)
+    f[1] = x[1] * (a1 + b1*x[2])
+    f[2] = x[2] * (a2 + b2*x[1])
+    nothing
+end
+
+function lotka_volterra_2d_ode(; x₀=[2., 1.], a1=-2, b1=+1, a2=+1, b2=-1)
+    function lotka_volterra_2d_ode_f(t, x, f)
+       lotka_volterra_2d_ode_f_params(t, x, f, a1, b1, a2, b2)
+    end
+    ODE(lotka_volterra_2d_ode_f_params, x₀)
+end
+
+
+function lotka_volterra_2d_iode_p(t, q, v, p)
+    p[1] = q[2] + log(q[2]) / q[1]
+    p[2] = q[1]
+    nothing
+end
+
+function lotka_volterra_2d_iode_f(t, q, v, f)
+    f[1] = v[2] - log(q[2]) * v[1] / q[1]^2 - 1. + 1. / q[1]
+    f[2] = v[1] * (1. + q[1] * q[2]) / (q[1] * q[2]) - 1. + 2. / q[2]
+    nothing
+end
+
+function lotka_volterra_2d_iode(q₀=[1., 1.], p₀=[1. + log(1.), 1.])
+    IODE(lotka_volterra_2d_iode_f, lotka_volterra_2d_iode_p, q₀, p₀)
+end
+
+
+function lotka_volterra_2d_idae_u(t, q, p, λ, u)
+    u[1] = λ[1]
+    u[2] = λ[2]
+    nothing
+end
+
+function lotka_volterra_2d_idae_g(t, q, p, λ, g)
+    g[1] = λ[2] - λ[1] * log(q[2]) / q[1]^2
+    g[2] = λ[1] * (1. + 1. / (q[1] * q[2]) )
+    nothing
+end
+
+function lotka_volterra_2d_idae_ϕ(t, q, p, ϕ)
+    ϕ[1] = p[1] - q[2] - log(q[2]) / q[1]
+    ϕ[2] = p[2] - q[1]
+    nothing
+end
+
+function lotka_volterra_2d_idae(q₀=[1., 1.], p₀=[1. + log(1.), 1.], λ₀ = [0.0, 0.0])
+    IDAE(lotka_volterra_2d_iode_f, lotka_volterra_2d_iode_p, lotka_volterra_2d_idae_u, lotka_volterra_2d_idae_g, lotka_volterra_2d_idae_ϕ, q₀, p₀, λ₀)
+end
