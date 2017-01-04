2c23bfdf69f85eb1081bb045c846571c5ca60ae5
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-01-04 10:53:06 +0900

    Cleanup in pendulum example problem.

--- src/problems/pendulum.jl
+++ src/problems/pendulum.jl
@@ -28,11 +28,13 @@ end
 function pendulum_iode_p(t, q, v, p)
     p[1] = q[2]
     p[2] = 0
+    nothing
 end
 
 function pendulum_iode_f(t, q, v, f)
     f[1] = sin(q[1])
     f[2] = v[1] - q[2]
+    nothing
 end
 
 function pendulum_iode(q₀=[acos(0.4), 0.0], p₀=[0.0, 0.0])
@@ -43,18 +45,21 @@ end
 function pendulum_idae_u(t, q, p, λ, u)
     u[1] = λ[1]
     u[2] = λ[2]
+    nothing
 end
 
 function pendulum_idae_g(t, q, p, λ, g)
     g[1] = 0
     g[2] = λ[1]
+    nothing
 end
 
 function pendulum_idae_ϕ(t, q, p, ϕ)
     ϕ[1] = p[1] - q[2]
     ϕ[2] = p[2]
+    nothing
 end
 
-function pendulum_idae(q₀=[acos(0.4), 0.0], p₀=[0.0, 0.0], λ₀ = [0.0, 0.0])
+function pendulum_idae(q₀=[acos(0.4), 0.0], p₀=[0.0, 0.0], λ₀=[0.0, 0.0])
     IDAE(pendulum_iode_f, pendulum_iode_p, pendulum_idae_u, pendulum_idae_g, pendulum_idae_ϕ, q₀, p₀, λ₀)
 end
