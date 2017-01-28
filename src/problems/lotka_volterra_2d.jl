bb3f858397b955c7de28b1018c786c600df2727a
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-01-28 22:45:35 +0900

    Fix in Lotka-Volterra 2d example.

--- src/problems/lotka_volterra_2d.jl
+++ src/problems/lotka_volterra_2d.jl
@@ -5,11 +5,11 @@ function lotka_volterra_2d_ode_f_params(t, x, f, a1, b1, a2, b2)
     nothing
 end
 
-function lotka_volterra_2d_ode(; x₀=[2., 1.], a1=-2, b1=+1, a2=+1, b2=-1)
+function lotka_volterra_2d_ode(x₀=[1., 1.]; a1=-2., b1=+1., a2=+1., b2=-1.)
     function lotka_volterra_2d_ode_f(t, x, f)
        lotka_volterra_2d_ode_f_params(t, x, f, a1, b1, a2, b2)
     end
-    ODE(lotka_volterra_2d_ode_f_params, x₀)
+    ODE(lotka_volterra_2d_ode_f, x₀)
 end
 
 
@@ -20,8 +20,8 @@ function lotka_volterra_2d_iode_p(t, q, v, p)
 end
 
 function lotka_volterra_2d_iode_f(t, q, v, f)
-    f[1] = v[2] - log(q[2]) * v[1] / q[1]^2 - 1. + 1. / q[1]
-    f[2] = v[1] * (1. + q[1] * q[2]) / (q[1] * q[2]) - 1. + 2. / q[2]
+    f[1] = v[2] - log(q[2]) * v[1] / q[1]^2 - 1 + 1 / q[1]
+    f[2] = v[1] * (1 + q[1] * q[2]) / (q[1] * q[2]) - 1 + 2 / q[2]
     nothing
 end
 
@@ -38,7 +38,7 @@ end
 
 function lotka_volterra_2d_idae_g(t, q, p, λ, g)
     g[1] = λ[2] - λ[1] * log(q[2]) / q[1]^2
-    g[2] = λ[1] * (1. + 1. / (q[1] * q[2]) )
+    g[2] = λ[1] * (1 + 1 / (q[1] * q[2]) )
     nothing
 end
 
@@ -48,6 +48,14 @@ function lotka_volterra_2d_idae_ϕ(t, q, p, ϕ)
     nothing
 end
 
+function lotka_volterra_2d_idae_v(t, q, p, v)
+    v[1] = q[1] * (q[2] - 2)
+    v[2] = q[2] * (1 - q[1])
+    nothing
+end
+
 function lotka_volterra_2d_idae(q₀=[1., 1.], p₀=[1. + log(1.), 1.], λ₀ = [0.0, 0.0])
-    IDAE(lotka_volterra_2d_iode_f, lotka_volterra_2d_iode_p, lotka_volterra_2d_idae_u, lotka_volterra_2d_idae_g, lotka_volterra_2d_idae_ϕ, q₀, p₀, λ₀)
+    IDAE(lotka_volterra_2d_iode_f, lotka_volterra_2d_iode_p,
+         lotka_volterra_2d_idae_u, lotka_volterra_2d_idae_g,
+         lotka_volterra_2d_idae_ϕ, lotka_volterra_2d_idae_v, q₀, p₀, λ₀)
 end
