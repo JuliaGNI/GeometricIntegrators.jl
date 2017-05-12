b5ee65351cb0e5e674b59170e699c3c5328378a3
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-05-12 10:23:50 +0200

    Added variational ODE.

--- src/Equations.jl
+++ src/Equations.jl
@@ -9,6 +9,7 @@ module Equations
     include("equations/ode.jl")
     include("equations/iode.jl")
     include("equations/pode.jl")
+    include("equations/vode.jl")
     include("equations/dae.jl")
     include("equations/idae.jl")
     include("equations/pdae.jl")
