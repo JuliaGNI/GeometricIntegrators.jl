6afef5504c535b52d93ebe98ede7c594b580a6de
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-05-16 12:32:01 +0200

    Update Problems module.

--- src/Problems.jl
+++ src/Problems.jl
@@ -2,25 +2,12 @@ __precompile__()
 
 module Problems
 
-    export ChargedParticle3dSingular, ChargedParticle3dSymmetric,
-           ChargedParticle3dUniform, GuidingCenter4dTokamakBarelyTrapped,
-           GuidingCenter4dTokamakPassing, GuidingCenter4dTokamakTrapped,
-           GuidingCenter4dTokamakLoop, GuidingCenter4dTokamakSurface,
-           GuidingCenter4dSymmetricLoop, GuidingCenter4dSymmetricSurface,
-           GuidingCenter4dUniformLoop,
+    export ChargedParticle3dSingular, ChargedParticle3dSymmetric, ChargedParticle3dUniform,
            ExponentialGrowth, LotkaVolterra2d, Pendulum, PointVortices
 
     include("problems/charged_particle_3d_singular.jl")
     include("problems/charged_particle_3d_symmetric.jl")
     include("problems/charged_particle_3d_uniform.jl")
-    include("problems/guiding_center_4d_tokamak_barely_trapped.jl")
-    include("problems/guiding_center_4d_tokamak_passing.jl")
-    include("problems/guiding_center_4d_tokamak_trapped.jl")
-    include("problems/guiding_center_4d_tokamak_loop.jl")
-    include("problems/guiding_center_4d_tokamak_surface.jl")
-    include("problems/guiding_center_4d_uniform_loop.jl")
-    include("problems/guiding_center_4d_symmetric_loop.jl")
-    include("problems/guiding_center_4d_symmetric_surface.jl")
     include("problems/exponential_growth.jl")
     include("problems/lotka_volterra_2d.jl")
     include("problems/pendulum.jl")
