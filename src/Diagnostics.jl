e8661506f14a87a1f6298d2f741d473a3661bdeb
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-07-17 16:32:58 +0200

    Cleanup 1st Poincare invariant diagnostics.

--- src/Diagnostics.jl
+++ src/Diagnostics.jl
@@ -2,6 +2,9 @@ __precompile__()
 
 module Diagnostics
 
+    using HDF5
+
+    using ..CommonFunctions
     using ..BasisFunctions
     using ..Equations
     using ..Integrators
@@ -10,7 +13,7 @@ module Diagnostics
     using ..Utils
 
     export PoincareInvariant1st, PoincareInvariant2nd, PoincareInvariant2ndTrapezoidal,
-           evaluate_poincare_invariant
+           evaluate_poincare_invariant, write_to_hdf5
 
     include("diagnostics/poincare_invariant_1st.jl")
     include("diagnostics/poincare_invariant_2nd.jl")
--- src/diagnostics/poincare_invariant_1st.jl
+++ src/diagnostics/poincare_invariant_1st.jl
@@ -7,14 +7,13 @@ struct PoincareInvariant1st{ET,DT,TT,ΘT}
     ntime::Int
     nsave::Int
     nplot::Int
-    odir::String
-    atol::DT
-    rtol::DT
-    nmax::Int
+    nt::Int
+    I::Vector{DT}
+    J::Vector{DT}
+    L::Vector{DT}
 end
 
-function PoincareInvariant1st(equ::Equation{DT,TT}, Θ::ΘT, Δt::TT, nloop::Int, ntime::Int, nsave::Int, nplot::Int, odir::String;
-                              atol::DT=2*eps(), rtol::DT=2*eps(), nmax::Int=100) where {DT,TT,ΘT}
+function PoincareInvariant1st(equ::Equation{DT,TT}, Θ::ΘT, Δt::TT, nloop::Int, ntime::Int, nsave::Int, nplot::Int) where {DT,TT,ΘT}
 
     println()
     println("First Euler-Poincaré Integral Invariant")
@@ -22,53 +21,62 @@ function PoincareInvariant1st(equ::Equation{DT,TT}, Θ::ΘT, Δt::TT, nloop::Int
     println()
     println(" nloop = ", nloop)
     println(" ntime = ", ntime)
+    println(" nsave = ", nsave)
     println(" Δt    = ", Δt)
     println()
 
-    if !isdir(odir)
-        mkpath(odir)
-    end
+    nt = div(ntime, nsave)
 
-    PoincareInvariant1st{typeof(equ),DT,TT,ΘT}(equ, Θ, Δt, nloop, ntime, nsave, nplot, odir, atol, rtol, nmax)
+    I = zeros(DT, nt+1)
+    J = zeros(DT, nt+1)
+    L = zeros(DT, nt+1)
+
+    PoincareInvariant1st{typeof(equ),DT,TT,ΘT}(equ, Θ, Δt, nloop, ntime, nsave, nplot, nt, I, J, L)
 end
 
 
-
-function evaluate_poincare_invariant(pinv::PoincareInvariant1st, integrator, tableau, runid)
-
-    int = integrator(pinv.equ, tableau, pinv.Δt; atol=pinv.atol, rtol=pinv.rtol, nmax=pinv.nmax)
-    sol = Solution(pinv.equ, pinv.Δt, pinv.ntime, pinv.nsave)
-
-    println("Running ", tableau.name, " (", runid, ")...")
-
-    integrate!(int, sol)
-    # try
-    #     integrate!(int, sol)
-    # catch DomainError
-    #     println("DOMAIN ERROR")
-    # end
-
-    I = zeros(sol.t.n+1)
-    J = zeros(sol.t.n+1)
+function evaluate_poincare_invariant(pinv::PoincareInvariant1st, sol::Solution)
     p = zeros(sol.q.d)
+    g = zeros(sol.q.d)
     v = zeros(size(sol.q.d,1), size(sol.q.d,3))
+    γ = zeros(size(sol.q.d,1), size(sol.q.d,3))
 
     compute_one_form(sol.t.t, sol.q.d, p, pinv.Θ)
 
-    for i in 1:size(sol.q.d,2)
-        compute_velocity(sol.q.d[:,i,:], v)
-        I[i] = compute_loop_integral(p[:,i,:], v)
-        J[i] = compute_loop_integral(sol.p.d[:,i,:], v)
+    if isdefined(sol, :λ)
+        compute_correction(sol.t.t, sol.q.d, sol.λ.d, g, pinv.equ.g)
     end
 
-    plot_integral_error(sol.t.t, I, pinv.odir * "/" * runid * "_poincare_1st_q.png";
-                        plot_title=L"$\Delta \int_\gamma \theta_i (q) \, dq^i$")
-    plot_integral_error(sol.t.t, J, pinv.odir * "/" * runid * "_poincare_1st_p.png";
-                        plot_title=L"$\Delta \int_\gamma p_i \, dq^i$")
-    plot_loop(sol, pinv.nplot, pinv.odir * "/" * runid * "_loop.png")
+    for i in 1:size(sol.q.d,2)
+        compute_velocity(sol.q.d[:,i,:], v)
+        pinv.I[i] = compute_loop_integral(p[:,i,:], v)
 
+        if isdefined(sol, :p)
+            pinv.J[i] = compute_loop_integral(sol.p.d[:,i,:], v)
+        end
+
+        if isdefined(sol, :λ)
+            compute_velocity(sol.λ.d[:,i,:], γ)
+            pinv.L[i] = compute_loop_integral(p[:,i,:] .- pinv.Δt .* g[:,i,:], v .- pinv.Δt .* γ)
+        end
+    end
+
+    (pinv.I, pinv.J, pinv.L)
 end
 
+
+function CommonFunctions.write_to_hdf5(pinv::PoincareInvariant1st, sol::Solution, output_file::String)
+    # h5open(output_file, isfile(output_file) ? "r+" : "w") do h5
+    h5open(output_file, "w") do h5
+
+        write(h5, "t", sol.t.t)
+        write(h5, "I", pinv.I)
+
+        isdefined(sol, :p) ? write(h5, "J", pinv.J) : nothing
+        isdefined(sol, :λ) ? write(h5, "L", pinv.L) : nothing
+
+    end
+end
 
 
 function compute_one_form(t, x, p, Θ)
@@ -95,6 +103,21 @@ function compute_velocity(γ, γ̇)
     for l in 1:size(γ,1)
         γ̂ = rfft(γ[l,:])
         γ̇[l,:] .= irfft(k .* γ̂, n) / n
+    end
+end
+
+
+function compute_correction(t, x, λ, σ, g)
+    @assert size(x) == size(λ) == size(σ)
+    @assert length(t) == size(x,2) == size(σ,2)
+
+    tσ = zeros(eltype(x), size(x,1))
+
+    for k in 1:size(x,3)
+        for j in 1:size(x,2)
+            g(t[j], x[:,j,k], λ[:,j,k], tσ)
+            σ[:,j,k] .= tσ
+        end
     end
 end
 
