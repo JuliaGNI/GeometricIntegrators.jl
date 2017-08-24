682a3257b13febad197db0c5de519d1466b96e28
Michael Kraus <michael.kraus@ipp.mpg.de>
2017-08-24 16:29:38 +0200

    Add (optional) initial value for lambda to IODE.

--- src/equations/iode.jl
+++ src/equations/iode.jl
@@ -74,26 +74,34 @@ struct IODE{dType <: Number, tType <: Number, αType <: Function, fType <: Funct
     t₀::tType
     q₀::Array{dType, N}
     p₀::Array{dType, N}
+    λ₀::Array{dType, N}
     periodicity::Vector{dType}
 
-    function IODE{dType,tType,αType,fType,gType,vType,N}(d, n, α, f, g, v, t₀, q₀, p₀; periodicity=[]) where {dType <: Number, tType <: Number, αType <: Function, fType <: Function, gType <: Function, vType <: Function, N}
-        @assert d == size(q₀,1) == size(p₀,1)
-        @assert n == size(q₀,2) == size(p₀,2)
-        @assert dType == eltype(q₀) == eltype(p₀)
-        @assert ndims(q₀) == ndims(p₀) == N ∈ (1,2)
+    function IODE{dType,tType,αType,fType,gType,vType,N}(d, n, α, f, g, v, t₀, q₀, p₀, λ₀; periodicity=[]) where {dType <: Number, tType <: Number, αType <: Function, fType <: Function, gType <: Function, vType <: Function, N}
+        @assert d == size(q₀,1) == size(p₀,1) == size(λ₀,1)
+        @assert n == size(q₀,2) == size(p₀,2) == size(λ₀,2)
+        @assert dType == eltype(q₀) == eltype(p₀) == eltype(λ₀)
+        @assert ndims(q₀) == ndims(p₀) == ndims(λ₀) == N ∈ (1,2)
 
         if !(length(periodicity) == d)
             periodicity = zeros(dType, d)
         end
 
-        new(d, d, n, α, f, g, v, t₀, q₀, p₀, periodicity)
+        new(d, d, n, α, f, g, v, t₀, q₀, p₀, λ₀, periodicity)
     end
 end
 
-
-function IODE(α::ΑT, f::FT, g::GT, v::VT, t₀::TT, q₀::DenseArray{DT}, p₀::DenseArray{DT}; periodicity=[]) where {DT,TT,ΑT,FT,GT,VT}
+function IODE(α::ΑT, f::FT, g::GT, v::VT, t₀::TT, q₀::DenseArray{DT}, p₀::DenseArray{DT}, λ₀::DenseArray{DT}; periodicity=[]) where {DT,TT,ΑT,FT,GT,VT}
     @assert size(q₀) == size(p₀)
-    IODE{DT, TT, ΑT, FT, GT, VT, ndims(q₀)}(size(q₀, 1), size(q₀, 2), α, f, g, v, t₀, q₀, p₀, periodicity=periodicity)
+    IODE{DT, TT, ΑT, FT, GT, VT, ndims(q₀)}(size(q₀, 1), size(q₀, 2), α, f, g, v, t₀, q₀, p₀, λ₀, periodicity=periodicity)
+end
+
+function IODE(α::Function, f::Function, g::Function, v::Function, q₀::DenseArray, p₀::DenseArray, λ₀::DenseArray; periodicity=[])
+    IODE(α, f, g, v, zero(eltype(q₀)), q₀, p₀, λ₀, periodicity=periodicity)
+end
+
+function IODE(α::Function, f::Function, g::Function, v::Function, t₀::Number, q₀::DenseArray, p₀::DenseArray; periodicity=[])
+    IODE(α, f, g, v, t₀, q₀, p₀, zeros(q₀), periodicity=periodicity)
 end
 
 function IODE(α::Function, f::Function, g::Function, t₀::Number, q₀::DenseArray, p₀::DenseArray; periodicity=[])
@@ -119,4 +127,5 @@ Base.:(==)(ode1::IODE, ode2::IODE) = (
                              && ode1.t₀ == ode2.t₀
                              && ode1.q₀ == ode2.q₀
                              && ode1.p₀ == ode2.p₀
+                             && ode1.λ₀ == ode2.λ₀
                              && ode1.periodicity == ode2.periodicity)
