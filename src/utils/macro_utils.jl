d16d8ebbfef2ece8eefa3989edab3c9ef2338554
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-02-18 12:08:43 +0100

    Remove reexport macro and use Reexport package instead.

--- src/utils/macro_utils.jl
+++ src/utils/macro_utils.jl
@@ -12,29 +12,6 @@ macro define(name, definition)
 end
 
 
-macro reexport(ex)
-    isa(ex, Expr) && (ex.head == :module ||
-                      ex.head == :using ||
-                      ex.head == :importall ||
-                      (ex.head == :toplevel &&
-                       all(e->isa(e, Expr) && (e.head == :using || e.head == :importall), ex.args))) ||
-        error("@reexport: syntax error")
-
-    if ex.head == :module
-        modules = Any[ex.args[2]]
-        ex = Expr(:toplevel, ex, :(using .$(ex.args[2])))
-    elseif (ex.head == :using || ex.head == :importall) && all(e->isa(e, Symbol), ex.args)
-        modules = Any[ex.args[end]]
-    else
-        modules = Any[e.args[end] for e in ex.args]
-    end
-
-    # TODO Replace 'GeometricIntegrators' with name of module from which 'reexport()' is called.
-    esc(Expr(:toplevel, ex,
-             [:(Core.eval(GeometricIntegrators, Expr(:export, names($mod)...))) for mod in modules]...))
-end
-
-
 function _dec128(x::Expr)
     y = x
     y.args .= _dec128.(y.args)
