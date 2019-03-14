a2b38fd2556eab28356da082d1f65193135c8a6a
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-03-14 11:26:14 +0100

    Fix Travis docs script.

--- docs/travis.sh
+++ docs/travis.sh
@@ -2,5 +2,5 @@
 
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
     julia --color=yes -e 'Pkg.add("Documenter")';
-    julia --color=yes -e 'cd(Pkg.dir("PoincareInvariants")); include(joinpath("docs", "make.jl"))';
+    julia --color=yes -e 'cd(Pkg.dir("GeometricIntegrators")); include(joinpath("docs", "make.jl"))';
 fi
