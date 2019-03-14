b536478ddee5573f3b99b9292a0c93d7ca4a5b5b
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-03-14 14:16:32 +0100

    Cleanup timeseries tests and add some new tests.

--- test/solutions/timeseries_tests.jl
+++ test/solutions/timeseries_tests.jl
@@ -4,6 +4,11 @@ ntime = 10
 
 ts = TimeSeries{eltype(Δt)}(ntime, Δt, 1)
 @test typeof(ts) <: AbstractArray
+@test lastindex(ts) == ntime
+@test lastindex(ts,1) == lastindex(ts.t,1) - 1
+@test axes(ts) == (0:ntime,)
+@test axes(ts,1) == 0:ntime
+@test size(ts.t) == (ntime+1,)
 
 compute_timeseries!(ts, 0.)
 t = collect(0:Δt:ntime*Δt)
