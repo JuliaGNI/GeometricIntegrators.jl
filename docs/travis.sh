8f5e8314857c6e0ffffa22bd098f82267a26ed8b
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-03-14 21:39:08 +0100

    Fix deprecation in Travis docs script.

--- docs/travis.sh
+++ docs/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-    julia --color=yes -e 'cd(Pkg.dir("GeometricIntegrators")); include(joinpath("docs", "make.jl"))';
+    julia --color=yes -e 'import GeometricIntegrators; include(joinpath(dirname(pathof(GeometricIntegrators)), "..", "docs", "make.jl"))';
 fi
