c9fc2a85f1c8b16d52f9ee0562528b27a3ff6636
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-03-15 12:25:53 +0100

    Reactivated and extended nonlinear solver tests.

--- test/solvers/nonlinear_solvers_tests.jl
+++ test/solvers/nonlinear_solvers_tests.jl
@@ -1,27 +1,30 @@
 
-# TODO (reactivate)
+n = 1
+T = Float64
 
-# n = 1
-# T = Float64
-#
-# function F(x::Vector, b::Vector)
-#     b[:] = x.^2
-# end
-#
-# function J(x::Vector, A::Matrix)
-#     A[:,:] = 2x
-# end
-#
-#
-# x = ones(T, n)
-# nl = NewtonSolver(x, F)
-# solve!(nl)
-# # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
-# @test_approx_eq_eps(nl.x, zeros(T, n), 1E-7)
-#
-#
-# x = ones(T, n)
-# nl = NewtonSolver(x, F, J=J)
-# solve!(nl)
-# # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
-# @test_approx_eq_eps(nl.x, zeros(T, n), 1E-7)
+function F(x::Vector, b::Vector)
+    b[:] = x.^2
+end
+
+function J(x::Vector, A::Matrix)
+    A[:,:] = 2x
+end
+
+
+for Solver in (NewtonSolver, QuasiNewtonSolver)
+    x = ones(T, n)
+    nl = Solver(x, F)
+    solve!(nl)
+    # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
+    for x in nl.x
+        @test x ≈ 0 atol=1E-7
+    end
+
+    x = ones(T, n)
+    nl = Solver(x, F, J=J)
+    solve!(nl)
+    # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
+    for x in nl.x
+        @test x ≈ 0 atol=1E-7
+    end
+end
