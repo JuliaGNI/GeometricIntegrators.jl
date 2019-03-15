2b6dba106a74b928539f5f6be8f825c56b183afb
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-03-15 13:20:50 +0100

    Add tests for Jacobians.

--- test/runtests.jl
+++ test/runtests.jl
@@ -36,6 +36,9 @@ function gϕ(t, q, p, gϕ)
 end
 
 
+include("solvers/linear_solvers_tests.jl")
+include("solvers/jacobian_tests.jl")
+include("solvers/nonlinear_solvers_tests.jl")
 include("basis_functions/vandermonde_tests.jl")
 include("basis_functions/lagrange_basis_tests.jl")
 include("basis_functions/lagrange_polynomials_tests.jl")
@@ -46,7 +49,5 @@ include("solutions/timeseries_tests.jl")
 include("solutions/solutions_tests.jl")
 include("tableaus/tableaus_tests.jl")
 include("integrators/integrators_test.jl")
-include("solvers/linear_solvers_tests.jl")
-include("solvers/nonlinear_solvers_tests.jl")
 
 #include("utils/hdf5_tests.jl")
