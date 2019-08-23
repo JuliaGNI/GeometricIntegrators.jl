a3e923102da84a460c9b3dcdb94f9b9ef9c6933c
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-08-24 01:36:57 +0900

    Move midpoint extrapolation for partitioned ODEs from initial_guess_pode.jl to extrapolation.jl.

--- src/integrators/initial_guess/extrapolation.jl
+++ src/integrators/initial_guess/extrapolation.jl
@@ -109,3 +109,73 @@ function midpoint_extrapolation(v::Function, t₀::TT, t₁::TT, x₀::Vector{DT
 
     aitken_neville(σ², pts, zero(TT), x₁)
 end
+
+
+"""
+Midpoint extrapolation method with arbitrary order p.
+
+    v:  function to compute vector field
+    f:  function to compute force  field
+    t₀: initial time
+    t₁: final   time
+    q₀: initial positions
+    p₀: initial momenta
+    q₁: final   positions
+    p₁: final   momenta
+    s:  number of interpolations (order p=2s+2)
+"""
+function midpoint_extrapolation(v::Function, f::Function, t₀::TT, t₁::TT, q₀::Vector{DT}, q₁::Vector{DT}, p₀::Vector{DT}, p₁::Vector{DT}, s::Int) where {DT,TT}
+    @assert size(q₀) == size(q₁) == size(p₀) == size(p₁)
+
+    local F   = 2*collect(1:(s+1))
+    local Δt  = t₁ - t₀
+    local σ   = Δt ./ F
+    local σ2  = σ.^2
+
+    local qts = zeros(eltype(q₀), length(q₀), s+1)
+    local pts = zeros(eltype(p₀), length(p₀), s+1)
+
+    local qᵢ₁= zero(q₀)
+    local qᵢ₂= zero(q₀)
+    local qᵢₜ= zero(q₀)
+
+    local pᵢ₁= zero(p₀)
+    local pᵢ₂= zero(p₀)
+    local pᵢₜ= zero(p₀)
+
+    local v₀ = zero(q₀)
+    local vᵢ = zero(q₀)
+
+    local f₀ = zero(p₀)
+    local fᵢ = zero(p₀)
+
+    v(t₀, q₀, p₀, v₀)
+    f(t₀, q₀, v₀, f₀)
+
+    for i in 1:(s+1)
+        tᵢ   = t₀ + σ[i]
+        qᵢ₁ .= q₀
+        qᵢ₂ .= q₀ + σ[i] .* v₀
+        pᵢ₁ .= p₀
+        pᵢ₂ .= p₀ + σ[i] .* f₀
+        for j in 1:(F[i]-1)
+            v(tᵢ, qᵢ₂, pᵢ₂, vᵢ)
+            f(tᵢ, qᵢ₂, vᵢ,  fᵢ)
+            qᵢₜ .= qᵢ₁ + 2σ[i] .* vᵢ
+            qᵢ₁ .= qᵢ₂
+            qᵢ₂ .= qᵢₜ
+            pᵢₜ .= pᵢ₁ + 2σ[i] .* fᵢ
+            pᵢ₁ .= pᵢ₂
+            pᵢ₂ .= pᵢₜ
+        end
+        for k in axes(qts,1)
+            qts[k,i] += qᵢ₂[k]
+        end
+        for k in axes(pts,1)
+            pts[k,i] += pᵢ₂[k]
+        end
+    end
+
+    aitken_neville(σ2, qts, zero(TT), q₁)
+    aitken_neville(σ2, pts, zero(TT), p₁)
+end
