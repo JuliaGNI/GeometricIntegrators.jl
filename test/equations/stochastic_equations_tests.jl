e3db80d37d4bce1f97b3616495e126bf29ff4dd4
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-11-12 14:25:46 +0100

    Split equation tests.

---
+++ test/equations/stochastic_equations_tests.jl
@@ -0,0 +1,31 @@
+
+@testset "$(rpad("Stochastic equations",80))" begin
+
+    ################################################################################
+    # Test SDE: Stochastic Differential Equation
+    ################################################################################
+
+    function v_sde(λ, t, q, v)
+        v[1] = λ*q[1]
+        v[2] = λ*q[2]
+    end
+
+    function u_sde(μ, t, q, u)
+        u[1] = μ*q[1]
+        u[2] = μ*q[2]
+    end
+
+    λ  = 2.
+    μ  = 1.
+
+    v_sde_params = (t, q, v) -> v_sde(λ, t, q, v)
+    u_sde_params = (t, q, v) -> u_sde(μ, t, q, v)
+
+    sde  = SDE{eltype(x₀), typeof(t₀), typeof(v_sde_params), typeof(u_sde_params), 1}(2, 1, 1, 1, v_sde_params, u_sde_params, t₀, x₀)
+    sde1 = SDE(1, 1, v_sde_params, u_sde_params, t₀, x₀)
+    sde2 = SDE(1, 1, v_sde_params, u_sde_params, x₀)
+
+    @test sde == sde1
+    @test sde == sde2
+
+end
