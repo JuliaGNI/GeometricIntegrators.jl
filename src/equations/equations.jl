539812804baae925cdc11cb84d0d8c8b519ee64a
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-11-12 15:45:51 +0100

    Add tests for general equation functionality.

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -13,6 +13,6 @@ function get_λ₀(q₀::AbstractArray{DT,2}, λ₀) where {DT}
     zeros(DT, size(λ₀,1), size(q₀,2))
 end
 
-Base.ndims(equ::Equation) = error("ndims() not implemented for ", typeof(equ))
+Base.ndims(equ::Equation) = error("ndims() not implemented for ", typeof(equ), ".")
 
 # TODO Add functions and vectors for invariants (momentum maps, energy, ...).
--- test/equations/equations_tests.jl
+++ test/equations/equations_tests.jl
@@ -1,5 +1,6 @@
 
 using GeometricIntegrators.Equations
+using GeometricIntegrators.Equations: function_v_dummy, get_λ₀
 using Test
 
 t₀ = 0.
@@ -7,6 +8,21 @@ q₀ = [1.]
 p₀ = [1.]
 x₀ = [1., 1.]
 λ₀ = [0.]
+
+
+@testset "$(rpad("General equation functionality",80))" begin
+
+    @test function_v_dummy(t₀, q₀, p₀, λ₀) == nothing
+
+    @test get_λ₀(zeros(3), zeros(3))   == zeros(3)
+    @test get_λ₀(zeros(3), zeros(3,3)) == zeros(3)
+    @test get_λ₀(zeros(3,3), zeros(3)) == zeros(3,3)
+
+    struct TestEquation{DT,TT} <: Equation{DT,TT} end
+
+    @test_throws ErrorException ndims(TestEquation{Float64,Float64}())
+
+end
 
 
 include("deterministic_equations_tests.jl")
