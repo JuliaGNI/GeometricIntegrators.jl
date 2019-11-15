243d502a617b129a4c953533fc570812b0abe57e
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-11-15 14:00:23 +0100

    Fix tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using SafeTestsets
 @safetestset "Solver Tests                                                                    " begin include("solvers/solvers_tests.jl") end
 @safetestset "Basis Function Tests                                                            " begin include("basis_functions/basis_functions_tests.jl") end
 @safetestset "Equation Tests                                                                  " begin include("equations/equations_tests.jl") end
-@safetestset "Solution Tests                                                                  " begin include("solution/solution_tests.jl") end
+@safetestset "Solution Tests                                                                  " begin include("solutions/solutions_tests.jl") end
 @safetestset "Tableau Tests                                                                   " begin include("tableaus/tableaus_tests.jl") end
 @safetestset "Integrator Tests                                                                " begin include("integrators/integrators_tests.jl") end
 
