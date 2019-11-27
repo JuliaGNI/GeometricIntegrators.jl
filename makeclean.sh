df1e3ba1e88c8f2f04deb564300d90014dba3ad9
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-11-27 21:53:38 +0100

    Update clean script to account for new *.mem file naming.

--- makeclean.sh
+++ makeclean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -type f -name '*.jl.mem' -exec rm {} +
+find . -type f -name '*.jl.*.mem' -exec rm {} +
 find . -type f -name '.DS_Store' -exec rm {} +
 find . -type d -name '.ipynb_checkpoints' -exec rm -Rf {} +
