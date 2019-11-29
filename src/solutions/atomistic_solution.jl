a4fb23e6fea3448d26cdb81a9c60663a42071e75
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-11-29 16:59:28 +0100

    Fix in atomistic solution constructors.

--- src/solutions/atomistic_solution.jl
+++ src/solutions/atomistic_solution.jl
@@ -6,23 +6,23 @@ abstract type AtomisticSolution{dType <: Number, tType <: Real} end
 
 
 "Create AtomisticSolution for ODE."
-function AtomisticSolution(equation::AbstractEquationODE)
-    AtomisticSolutionODE()
+function AtomisticSolution(equation::AbstractEquationODE{DT,TT}) where {DT,TT}
+    AtomisticSolutionODE{DT,TT}(ndims(equation))
 end
 
 "Create AtomisticSolution for partitioned ODE."
-function AtomisticSolution(equation::AbstractEquationPODE)
-    AtomisticSolutionPODE()
+function AtomisticSolution(equation::AbstractEquationPODE{DT,TT}) where {DT,TT}
+    AtomisticSolutionPODE{DT,TT}(ndims(equation))
 end
 
 "Create AtomisticSolution for DAE."
-function AtomisticSolution(equation::AbstractEquationDAE)
-    AtomisticSolutionDAE()
+function AtomisticSolution(equation::AbstractEquationDAE{DT,TT}) where {DT,TT}
+    AtomisticSolutionDAE{DT,TT}(ndims(equation))
 end
 
 "Create AtomisticSolution for partitioned DAE."
-function AtomisticSolution(equation::AbstractEquationPDAE)
-    AtomisticSolutionPDAE()
+function AtomisticSolution(equation::AbstractEquationPDAE{DT,TT}) where {DT,TT}
+    AtomisticSolutionPDAE{DT,TT}(ndims(equation))
 end
 
 "Print error for AtomisticSolutions of equations not implemented, yet."
