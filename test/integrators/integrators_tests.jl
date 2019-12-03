369bd2b45d734c9df15ed0a5745aa8e0fad211a4
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-12-03 11:01:38 +0100

    Restored stochastic integrators.

--- test/integrators/integrators_tests.jl
+++ test/integrators/integrators_tests.jl
@@ -6,4 +6,4 @@ using SafeTestsets
 @safetestset "SPARK Integrators                                                               " begin include("spark_integrators_tests.jl") end
 @safetestset "Splitting Integrators                                                           " begin include("splitting_integrators_tests.jl") end
 @safetestset "Galerkin Integrators                                                            " begin include("galerkin_integrators_tests.jl") end
-#@safetestset "Stochastic Integrators                                                          " begin include("stochastic_integrators_tests.jl") end
+@safetestset "Stochastic Integrators                                                          " begin include("stochastic_integrators_tests.jl") end
