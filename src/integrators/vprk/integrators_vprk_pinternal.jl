98201c603a242599f4a7a5860787c7c55cc11127
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-12-06 10:28:43 +0100

    Some fixes to VPRKpInternal integrator.

--- src/integrators/vprk/integrators_vprk_pinternal.jl
+++ src/integrators/vprk/integrators_vprk_pinternal.jl
@@ -114,10 +114,8 @@ function compute_stages_q_vprk!(q::Vector{ST}, Q::Vector{Vector{ST}}, V::Vector{
         for j in 1:S
             y1 += params.tab.q.b[j] * V[j][k]
             y2 += params.tab.q.b̂[j] * V[j][k]
-            if mod(S,2) == 0
-                y3 += params.tab.q.b[j] * Λ[j][k]
-                y4 += params.tab.q.b̂[j] * Λ[j][k]
-            end
+            # y3 += 0.5 * (1 - params.tab.R∞) * params.tab.q.b[j] * Λ[j][k]
+            # y4 += 0.5 * (1 - params.tab.R∞) * params.tab.q.b̂[j] * Λ[j][k]
         end
         q[k] = params.q̅[k] + params.Δt * (y1 + y2 + y3 + y4)
     end
@@ -183,8 +181,8 @@ function compute_rhs_vprk_projection!(b::Vector{ST}, p::Vector{ST},
         for j in 1:S
             z1 += params.tab.p.b[j] * F[j][k]
             z2 += params.tab.p.b̂[j] * F[j][k]
-            z3 += params.tab.p.b[j] * R[j][k]
-            z4 += params.tab.p.b̂[j] * R[j][k]
+            # z3 += 0.5 * (1 - params.tab.R∞) * params.tab.p.b[j] * R[j][k]
+            # z4 += 0.5 * (1 - params.tab.R∞) * params.tab.p.b̂[j] * R[j][k]
         end
         b[offset+k] = (p[k] - params.p̅[k]) - params.Δt * (z1 + z2 + z3 + z4)
     end
