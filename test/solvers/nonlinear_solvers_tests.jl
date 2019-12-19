1f6b317c6428b2e710aad227443007afaac3c798
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-12-19 10:12:08 +0100

    Make solver testsets SafeTestsets.

--- test/solvers/nonlinear_solvers_tests.jl
+++ test/solvers/nonlinear_solvers_tests.jl
@@ -1,34 +1,34 @@
 
-@testset "$(rpad("Nonlinear solvers",80))" begin
+using GeometricIntegrators.Solvers
+using Test
 
-    n = 1
-    T = Float64
 
-    function F(x::Vector, b::Vector)
-        b[:] = x.^2
+n = 1
+T = Float64
+
+function F(x::Vector, b::Vector)
+    b[:] = x.^2
+end
+
+function J(x::Vector, A::Matrix)
+    A[:,:] = 2x
+end
+
+
+for Solver in (NewtonSolver, QuasiNewtonSolver)
+    x = ones(T, n)
+    nl = Solver(x, F)
+    solve!(nl)
+    # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
+    for x in nl.x
+        @test x ≈ 0 atol=1E-7
     end
 
-    function J(x::Vector, A::Matrix)
-        A[:,:] = 2x
+    x = ones(T, n)
+    nl = Solver(x, F; J! = J)
+    solve!(nl)
+    # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
+    for x in nl.x
+        @test x ≈ 0 atol=1E-7
     end
-
-
-    for Solver in (NewtonSolver, QuasiNewtonSolver)
-        x = ones(T, n)
-        nl = Solver(x, F)
-        solve!(nl)
-        # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
-        for x in nl.x
-            @test x ≈ 0 atol=1E-7
-        end
-
-        x = ones(T, n)
-        nl = Solver(x, F, J=J)
-        solve!(nl)
-        # println(nl.status.i, ", ", nl.status.rₐ,", ",  nl.status.rᵣ,", ",  nl.status.rₛ)
-        for x in nl.x
-            @test x ≈ 0 atol=1E-7
-        end
-    end
-
 end
