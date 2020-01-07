6a01bdb1acb8cfd0ece3f10cf00abc737f234c7f
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-01-07 16:26:13 +0100

    Implement Jacobian (via AD) for FIRK integrators.

--- src/integrators/integrator_cache.jl
+++ src/integrators/integrator_cache.jl
@@ -9,9 +9,10 @@ abstract type PODEIntegratorCache{DT,D} <: IntegratorCache{DT,D} end
 abstract type PDAEIntegratorCache{DT,D} <: IntegratorCache{DT,D} end
 
 
+IntegratorCache(params::Parameters) = error("IntegratorCache(params) not implemented for ", typeof(params))
+
 create_integrator_cache(integrator::Integrator) = error("create_integrator_cache()! not implemented for ", typeof(integrator))
 
 initialize!(::Integrator, ::AtomicSolution) = nothing
 
 integrate_step!(integrator::Integrator, ::AtomicSolution) = error("integrate_step()! not implemented for ", typeof(integrator))
-
