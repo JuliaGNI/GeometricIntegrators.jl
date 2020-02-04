8c200ba2bc9acabf8f3acbc64614068aae0e5d7f
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-04 08:47:43 +0100

    Update Kubo oscillator profiling script.

--- prototyping/profile_kubo.jl
+++ prototyping/profile_kubo.jl
@@ -33,61 +33,49 @@ end
 # SERK
 profile_kubo(getTableauBurrageE1(), kubo_oscillator_sde_1())
 profile_kubo(getTableauBurrageE1(), kubo_oscillator_sde_2())
-profile_kubo(getTableauBurrageE1(), kubo_oscillator_sde_3())
 #
 # SIRK
 profile_kubo(getTableauStochasticGLRK(1), kubo_oscillator_sde_1())
 profile_kubo(getTableauStochasticGLRK(1), kubo_oscillator_sde_2())
-profile_kubo(getTableauStochasticGLRK(1), kubo_oscillator_sde_3())
 #
 # WERK
 profile_kubo(getTableauRosslerRS1(), kubo_oscillator_sde_1())
 profile_kubo(getTableauRosslerRS1(), kubo_oscillator_sde_2())
-profile_kubo(getTableauRosslerRS1(), kubo_oscillator_sde_3())
 
 # WIRK
 profile_kubo(getTableauSRKw1(), kubo_oscillator_sde_1())
 profile_kubo(getTableauSRKw1(), kubo_oscillator_sde_2())
-profile_kubo(getTableauSRKw1(), kubo_oscillator_sde_3())
 
 # SIPRK
 profile_kubo(getTableauStochasticStormerVerlet(), kubo_oscillator_psde_1())
 profile_kubo(getTableauStochasticStormerVerlet(), kubo_oscillator_psde_2())
-profile_kubo(getTableauStochasticStormerVerlet(), kubo_oscillator_psde_3())
 
 # SISPRK
 profile_kubo(getTableauModifiedStochasticStormerVerlet(), kubo_oscillator_spsde_1())
 profile_kubo(getTableauModifiedStochasticStormerVerlet(), kubo_oscillator_spsde_2())
-profile_kubo(getTableauModifiedStochasticStormerVerlet(), kubo_oscillator_spsde_3())
 
 
 
 # SERK
 # profile_kubo(getTableauBurrageE1(), kubo_oscillator_sde_1(), true)
 # profile_kubo(getTableauBurrageE1(), kubo_oscillator_sde_2(), true)
-# profile_kubo(getTableauBurrageE1(), kubo_oscillator_sde_3(), true)
 
 # SIRK
 # profile_kubo(getTableauStochasticGLRK(1), kubo_oscillator_sde_1(), true)
 # profile_kubo(getTableauStochasticGLRK(1), kubo_oscillator_sde_2(), true)
-# profile_kubo(getTableauStochasticGLRK(1), kubo_oscillator_sde_3(), true)
 
 # WERK
 # profile_kubo(getTableauRosslerRS1(), kubo_oscillator_sde_1(), true)
 # profile_kubo(getTableauRosslerRS1(), kubo_oscillator_sde_2(), true)
-# profile_kubo(getTableauRosslerRS1(), kubo_oscillator_sde_3(), true)
 
 # WIRK
 # profile_kubo(getTableauSRKw1(), kubo_oscillator_sde_1(), true)
 # profile_kubo(getTableauSRKw1(), kubo_oscillator_sde_2(), true)
-# profile_kubo(getTableauSRKw1(), kubo_oscillator_sde_3(), true)
 
 # SIPRK
 # profile_kubo(getTableauStochasticStormerVerlet(), kubo_oscillator_psde_1(), true)
 # profile_kubo(getTableauStochasticStormerVerlet(), kubo_oscillator_psde_2(), true)
-# profile_kubo(getTableauStochasticStormerVerlet(), kubo_oscillator_psde_3(), true)
 
 # SISPRK
 # profile_kubo(getTableauModifiedStochasticStormerVerlet(), kubo_oscillator_spsde_1(), true)
 # profile_kubo(getTableauModifiedStochasticStormerVerlet(), kubo_oscillator_spsde_2(), true)
-# profile_kubo(getTableauModifiedStochasticStormerVerlet(), kubo_oscillator_spsde_3(), true)
