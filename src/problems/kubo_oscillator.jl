4f1175e65b351ff6abaa1f88e17094e041b58cce
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-04 10:53:47 +0100

    Add additional set of Kubo oscillator test problems.

--- src/problems/kubo_oscillator.jl
+++ src/problems/kubo_oscillator.jl
@@ -4,6 +4,7 @@ module KuboOscillatorProblem
 
     export kubo_oscillator_sde_1, kubo_oscillator_psde_1, kubo_oscillator_spsde_1
     export kubo_oscillator_sde_2, kubo_oscillator_psde_2, kubo_oscillator_spsde_2
+    export kubo_oscillator_sde_3, kubo_oscillator_psde_3, kubo_oscillator_spsde_3
 
     q_init_A=[0.5, 0.0]
     q_init_B=[0.5 0.0 -0.5; 0.0 0.5 0.0]
@@ -38,10 +39,15 @@ module KuboOscillatorProblem
         SDE(1, 3, kubo_oscillator_sde_v, kubo_oscillator_sde_B, q₀)
     end
 
-
     function kubo_oscillator_sde_2(q₀=q_init_B)
         # q_init_B - interpreted as three random initial conditions
         # The 3 columns correspond to 3 sample paths
+        # 1-dimensional noise
+        SDE(1, 1, kubo_oscillator_sde_v, kubo_oscillator_sde_B, q₀)
+    end
+
+    function kubo_oscillator_sde_3(q₀=q_init_A)
+        # q_init_A - interpreted as one random initial conditions with one sample path
         # 1-dimensional noise
         SDE(1, 1, kubo_oscillator_sde_v, kubo_oscillator_sde_B, q₀)
     end
@@ -80,10 +86,15 @@ module KuboOscillatorProblem
         PSDE(1, 3, kubo_oscillator_psde_v, kubo_oscillator_psde_f, kubo_oscillator_psde_B, kubo_oscillator_psde_G, q₀, p₀)
     end
 
-
     function kubo_oscillator_psde_2(q₀=q_init_D, p₀=p_init_D)
         # q_init_D - interpreted as a single random initial condition
         # The 3 columns correspond to 3 sample paths
+        # 1-dimensional noise
+        PSDE(1, 1, kubo_oscillator_psde_v, kubo_oscillator_psde_f, kubo_oscillator_psde_B, kubo_oscillator_psde_G, q₀, p₀)
+    end
+
+    function kubo_oscillator_psde_3(q₀=q_init_C, p₀=p_init_C)
+        # q_init_C - interpreted as a single random initial condition with one sample path
         # 1-dimensional noise
         PSDE(1, 1, kubo_oscillator_psde_v, kubo_oscillator_psde_f, kubo_oscillator_psde_B, kubo_oscillator_psde_G, q₀, p₀)
     end
@@ -123,10 +134,15 @@ module KuboOscillatorProblem
         SPSDE(1, 3, kubo_oscillator_spsde_v, kubo_oscillator_spsde_f1, kubo_oscillator_spsde_f2, kubo_oscillator_spsde_B, kubo_oscillator_spsde_G1, kubo_oscillator_spsde_G2, q₀, p₀)
     end
 
-
     function kubo_oscillator_spsde_2(q₀=q_init_D, p₀=p_init_D)
         # q_init_D - interpreted as a single random initial condition
         # The 3 columns correspond to 3 sample paths
+        # 1-dimensional noise
+        SPSDE(1, 1, kubo_oscillator_spsde_v, kubo_oscillator_spsde_f1, kubo_oscillator_spsde_f2, kubo_oscillator_spsde_B, kubo_oscillator_spsde_G1, kubo_oscillator_spsde_G2, q₀, p₀)
+    end
+
+    function kubo_oscillator_spsde_3(q₀=q_init_C, p₀=p_init_C)
+        # q_init_C - interpreted as a single random initial condition with one sample path
         # 1-dimensional noise
         SPSDE(1, 1, kubo_oscillator_spsde_v, kubo_oscillator_spsde_f1, kubo_oscillator_spsde_f2, kubo_oscillator_spsde_B, kubo_oscillator_spsde_G1, kubo_oscillator_spsde_G2, q₀, p₀)
     end
