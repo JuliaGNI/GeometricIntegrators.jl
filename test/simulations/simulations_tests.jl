4092f9fcdcc097c93a9dc47591de57b4926ace06
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-07 16:06:58 +0100

    Test for identical results in serial and parallel simulations.

--- test/simulations/simulations_tests.jl
+++ test/simulations/simulations_tests.jl
@@ -9,65 +9,63 @@ set_config(:nls_stol_break, Inf)
 
 
 Δt = 0.1
-nt = 100000
+nt = 10000
+ns = 10
 
 nsave1 = 1
-nwrte1 = 1000
+nwrte1 = 100
 
 nsave2 = 10
-nwrte2 = 1000
+nwrte2 = 100
 
 h5file = "test.hdf5"
 
 
 tab = getTableauImplicitMidpoint()
-ode = harmonic_oscillator_ode()
+ode = harmonic_oscillator_ode(vcat(rand(1,ns), zeros(1,ns)))
 
 
-@testset "$(rpad("Serial Simulation",80))" begin
+# @testset "$(rpad("Serial Simulation",80))" begin
 
     sim1 = Simulation(ode, tab, Δt, "Harmonic Oscillator Test 1", h5file, nt; nsave=nsave1, nwrite=nwrte1)
     run!(sim1)
     @test isfile(h5file)
-    sol1 = SSolutionODE(h5file)
-    @test sol1.ntime == nt
-    @test sol1.nsave == nsave1
+    ssol1 = SSolutionODE(h5file)
+    @test ssol1.ntime == nt
+    @test ssol1.nsave == nsave1
     rm(h5file)
 
     sim2 = Simulation(ode, tab, Δt, "Harmonic Oscillator Test 2", h5file, nt; nsave=nsave2, nwrite=nwrte2)
     run!(sim2)
     @test isfile(h5file)
-    sol2 = SSolutionODE(h5file)
-    @test sol2.ntime == nt
-    @test sol2.nsave == nsave2
+    ssol2 = SSolutionODE(h5file)
+    @test ssol2.ntime == nt
+    @test ssol2.nsave == nsave2
     rm(h5file)
 
-end
+# end
 
 
+# @testset "$(rpad("Parallel Simulation (" * string(Threads.nthreads()) * " Threads)",80))" begin
 
-nt = 10000
-ns = 10
-
-ode = harmonic_oscillator_ode(vcat(rand(1,ns), zeros(1,ns)))
-
-
-@testset "$(rpad("Parallel Simulation (" * string(Threads.nthreads()) * " Threads)",80))" begin
-
-    sim1 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 1", h5file, nt; nsave=nsave1, nwrite=nwrte1)
+    sim1 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 1 (" * string(Threads.nthreads()) * " Threads)", h5file, nt; nsave=nsave1, nwrite=nwrte1)
     run!(sim1)
     @test isfile(h5file)
-    sol1 = SSolutionODE(h5file)
-    @test sol1.ntime == nt
-    @test sol1.nsave == nsave1
+    psol1 = SSolutionODE(h5file)
+    @test psol1.ntime == nt
+    @test psol1.nsave == nsave1
     rm(h5file)
 
-    sim2 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 2", h5file, nt; nsave=nsave2, nwrite=nwrte2)
+    sim2 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 2 (" * string(Threads.nthreads()) * " Threads)", h5file, nt; nsave=nsave2, nwrite=nwrte2)
     run!(sim2)
     @test isfile(h5file)
-    sol2 = SSolutionODE(h5file)
-    @test sol2.ntime == nt
-    @test sol2.nsave == nsave2
+    psol2 = SSolutionODE(h5file)
+    @test psol2.ntime == nt
+    @test psol2.nsave == nsave2
     rm(h5file)
 
-end
+# end
+
+
+@test psol1.q == ssol1.q
+@test psol2.q == ssol2.q
