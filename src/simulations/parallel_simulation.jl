850bbefdd857c7fb1394f8b3f34b6ba33877af13
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-07 14:40:37 +0100

    Make ParallelSimulation thread save.

--- src/simulations/parallel_simulation.jl
+++ src/simulations/parallel_simulation.jl
@@ -1,35 +1,47 @@
 
-struct ParallelSimulation{ET <: Equation, IT <: Integrator, ST <: Solution}
+struct ParallelSimulation{ET <: Equation, IT <: Tuple, ST <: Solution}
     equation::ET
-    integrator::IT
+    integrators::IT
     solution::ST
     ncycle::Int
     run_id::String
     filename::String
 end
 
-equation(sim::ParallelSimulation) = sim.equation
-integrator(sim::ParallelSimulation) = sim.integrator
-solution(sim::ParallelSimulation) = sim.solution
-cycles(sim::ParallelSimulation) = 1:sim.ncycle
-
-
-function ParallelSimulation(equ::ET, int::IT, sol::ST, run_id::String, filename::String) where {ET,IT,ST}
-    @assert mod(sol.ntime, sol.nwrite) == 0
-    ncycle = div(sol.ntime, sol.nwrite)
-    ParallelSimulation{ET,IT,ST}(equ, int, sol, ncycle, run_id, filename)
+@inline equation(sim::ParallelSimulation) = sim.equation
+@inline integrators(sim::ParallelSimulation) = sim.integrators
+@inline integrator(sim::ParallelSimulation, id) = sim.integrators[id]
+@inline solution(sim::ParallelSimulation) = sim.solution
+@inline cycles(sim::ParallelSimulation) = 1:sim.ncycle
+@inline CommonFunctions.eachsample(sim::ParallelSimulation) = eachsample(solution(sim))
+@inline CommonFunctions.eachsample(sim::ParallelSimulation, id::Int) = begin
+    nthreads = Threads.nthreads()
+    ns = nsamples(solution(sim))
+    ns_thread = div(ns, nthreads)
+    i1 = (id-1)*ns_thread + 1
+    i2 =  id   *ns_thread
+    id == nthreads ? (i1:ns) : (i1:i2)
 end
 
-function ParallelSimulation(equ::Equation, int::Integrator, Δt, run_id, filename, ntime; nsave=1, nwrite=1)
-    ParallelSimulation(equ, int, ParallelSolution(equ, Δt, ntime; nsave=nsave, nwrite=nwrite), run_id, filename)
+
+function ParallelSimulation(equ::ET, ints::IT, sol::ST, run_id::String, filename::String) where {ET,IT,ST}
+    @assert mod(sol.ntime, sol.nwrite) == 0
+    ncycle = div(sol.ntime, sol.nwrite)
+    ParallelSimulation{ET,IT,ST}(equ, ints, sol, ncycle, run_id, filename)
+end
+
+function ParallelSimulation(equ::Equation, ints::Tuple, Δt, run_id, filename, ntime; nsave=1, nwrite=1)
+    ParallelSimulation(equ, ints, ParallelSolution(equ, Δt, ntime; nsave=nsave, nwrite=nwrite), run_id, filename)
 end
 
 function ParallelSimulation(equ::Equation, tableau::AbstractTableau, Δt, run_id, filename, ntime; kwargs...)
-    ParallelSimulation(equ, Integrator(equ, tableau, Δt), Δt, run_id, filename, ntime; kwargs...)
+    ints = Tuple(Integrator(equ, tableau, Δt) for i in 1:Threads.nthreads())
+    ParallelSimulation(equ, ints, Δt, run_id, filename, ntime; kwargs...)
 end
 
 function ParallelSimulation(equ::Equation, integrator, tableau::AbstractTableau, Δt, run_id, filename, ntime; kwargs...)
-    ParallelSimulation(equ, integrator(equ, tableau, Δt), Δt, run_id, filename, ntime; kwargs...)
+    ints = Tuple(integrator(equ, tableau, Δt) for i in 1:Threads.nthreads())
+    ParallelSimulation(equ, ints, Δt, run_id, filename, ntime; kwargs...)
 end
 
 
@@ -43,7 +55,9 @@ function run!(sim::ParallelSimulation)
         # loop over integration cycles showing progress bar
         @showprogress 5 for c in cycles(sim)
             # loop over samples
-            Threads.@threads for m in eachsample(solution(sim))
+            Threads.@threads for m in eachsample(sim)
+                id = Threads.threadid()
+
                 # create atomic solution
                 asol = AtomicSolution(equation(sim))
 
@@ -51,11 +65,11 @@ function run!(sim::ParallelSimulation)
                 get_initial_conditions!(solution(sim), asol, m)
 
                 # initilize integrator
-                initialize!(integrator(sim), asol)
+                initialize!(integrator(sim, id), asol)
 
                 # loop over time steps
                 for n in eachtimestep(solution(sim))
-                    integrate!(integrator(sim), solution(sim), asol, m, n)
+                    integrate!(integrator(sim, id), solution(sim), asol, m, n)
                 end
             end
 
