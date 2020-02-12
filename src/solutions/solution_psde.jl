021424d2fdb432d84e5f57467b72086c7d67be01
TomaszTyranowski <37413021+TomaszTyranowski@users.noreply.github.com>
2020-02-12 23:33:17 +0100

    Fixed bugs in SolutionSDE and SolutionPSDE
    The constructors called with predetermined Wiener process arrays dW and dZ did not work properly when ns=1 and ni>1 in the equation argument.
    
    Also added these constructors in solution.jl

--- src/solutions/solution_psde.jl
+++ src/solutions/solution_psde.jl
@@ -138,7 +138,7 @@ for (TSolution, TDataSeries, Tdocstring) in
 
             @assert nm == size(dW,1)
             @assert ntime == size(dW,2)
-            @assert ns == size(dW,3)
+            @assert max(ni,ns) == size(dW,3)
 
             # Holds the Wiener process data for ALL computed time steps
             # Wiener process increments are prescribed by the arrays dW and dZ
