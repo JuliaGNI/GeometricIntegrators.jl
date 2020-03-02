395e41aec6a7fc993e7f0754ecb58d8e3f3466f6
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-03-02 17:48:35 +0100

    Minor fix in docs.

--- docs/make.jl
+++ docs/make.jl
@@ -24,6 +24,7 @@ makedocs(
                 "Linear Solvers"    => "modules/solvers_linear.md",
                 "Nonlinear Solvers" => "modules/solvers_nonlinear.md",
                 "Quadrature Rules"  => "modules/quadratures.md",
+                "Simulations"       => "modules/simulations.md",
                 "Solutions"         => "modules/solutions.md",
                 "Tableaus"          => "modules/tableaus.md"],
              ]
