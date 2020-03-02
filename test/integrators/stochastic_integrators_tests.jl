9840975e21fb1224fd9726af4a3073152e38d086
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-03-02 15:19:08 +0100

    Add test for stochastic integrators.

--- test/integrators/stochastic_integrators_tests.jl
+++ test/integrators/stochastic_integrators_tests.jl
@@ -13,8 +13,10 @@ set_config(:nls_stol_break, 1E3)
 
 include("utils.jl")
 
+ode    = kubo_oscillator_ode()
 sde1   = kubo_oscillator_sde_1()
 sde2   = kubo_oscillator_sde_2()
+sde3   = kubo_oscillator_sde_3()
 psde1  = kubo_oscillator_psde_1()
 psde2  = kubo_oscillator_psde_2()
 spsde1  = kubo_oscillator_spsde_1()
@@ -56,6 +58,16 @@ end
     integrate!(int, sol)
     @test rel_energy_err_sde(sol) < 1E-14
 
+    # check deterministic part of SIRK integrator to correspond to FIRK integrator
+    int_sto = Integrator(sde3, getTableauStochasticGLRK(1), Δt)
+    sol_sto = Solution(sde3, Δt, zeros(1, nt), zeros(1, nt), nt, conv=:strong)
+    integrate!(int_sto, sol_sto)
+
+    int_det = Integrator(ode, getTableauGLRK(1), Δt)
+    sol_det = Solution(ode, Δt, nt)
+    integrate!(int_det, sol_det)
+
+    @test sol_sto.q[:,end] ≈ sol_det.q[:,end] atol=1E-14
 end
 
 
@@ -115,5 +127,5 @@ end
     sol = Solution(spsde2, Δt, nt, conv=:strong)
     integrate!(int, sol)
     @test rel_energy_err_psde(sol) < 2E-2
-    
+
 end
