c475df6617d4aa748d78325d4392e0e8855004d9
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-03-11 14:05:05 +0100

    Some more code consolidation in SPARK integrators.

--- src/integrators/spark/integrators_hpark.jl
+++ src/integrators/spark/integrators_hpark.jl
@@ -208,39 +208,6 @@ end
 end
 
 
-function initial_guess!(int::IntegratorHPARK, sol::AtomicSolutionPDAE)
-    for i in eachstage(int)
-        evaluate!(int.iguess, sol.q, sol.p, sol.v, sol.f,
-                              sol.q̅, sol.p̅, sol.v̅, sol.f̅,
-                              int.cache.q̃, int.cache.p̃, int.cache.ṽ, int.cache.f̃,
-                              tableau(int).q.c[i], tableau(int).p.c[i])
-
-        for k in eachdim(int)
-            int.solver.x[2*(ndims(int)*(i-1)+k-1)+1] = (int.cache.q̃[k] - sol.q[k])/timestep(int)
-            int.solver.x[2*(ndims(int)*(i-1)+k-1)+2] = (int.cache.p̃[k] - sol.p[k])/timestep(int)
-        end
-    end
-
-    for i in 1:pstages(int)
-        evaluate!(int.iguess, sol.q, sol.p, sol.v, sol.f,
-                              sol.q̅, sol.p̅, sol.v̅, sol.f̅,
-                              int.cache.q̃, int.cache.p̃, int.cache.ṽ, int.cache.f̃,
-                              tableau(int).q̃.c[i], tableau(int).p̃.c[i])
-
-        for k in eachdim(int)
-            int.solver.x[2*ndims(int)*nstages(int)+3*(ndims(int)*(i-1)+k-1)+1] = (int.cache.q̃[k] - sol.q[k])/timestep(int)
-            int.solver.x[2*ndims(int)*nstages(int)+3*(ndims(int)*(i-1)+k-1)+2] = (int.cache.p̃[k] - sol.p[k])/timestep(int)
-            int.solver.x[2*ndims(int)*nstages(int)+3*(ndims(int)*(i-1)+k-1)+3] = 0
-        end
-    end
-
-    if int.params.t_λ.c[1] == 0
-        for k in 1:ndims(int)
-            int.solver.x[2*ndims(int)*nstages(int)+3*(k-1)+3] = int.cache.λ[k]
-        end
-    end
-end
-
 function update_solution!(int::IntegratorHPARK{DT,TT}, sol::AtomicSolutionPDAE{DT,TT}) where {DT,TT}
     # compute final update
     update_solution!(sol.q, sol.q̃, int.cache.Vi, int.params.t_q.b, timestep(int))
