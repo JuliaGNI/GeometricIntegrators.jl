294d725ed365114d72511e1db71f678c409d49d7
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-03-17 11:48:48 +0100

    First working prototype for new integrator cache mechanism. (+1 squashed commit) Squashed commits: [51c49f5] Minor cleanup in CacheDict.
    First working prototype for new integrator cache mechanism. (+1 squashed commit)
    Squashed commits:
    [f6b93c2] Make CacheDict thread-safe.
    
    First working prototype for new integrator cache mechanism. (+1 squashed commit)
    Squashed commits:
    [51c49f5] Minor cleanup in CacheDict.
    
    First working prototype for new integrator cache mechanism.

--- src/integrators/integrator_cache.jl
+++ src/integrators/integrator_cache.jl
@@ -11,4 +11,23 @@ abstract type PDAEIntegratorCache{DT,D} <: IntegratorCache{DT,D} end
 
 IntegratorCache(params::Parameters) = error("IntegratorCache(params) not implemented for ", typeof(params))
 IntegratorCache(integrator::Integrator) = error("IntegratorCache(int)! not implemented for ", typeof(integrator))
+CacheType(T, params::Parameters) = error("CacheType(T, params) not implemented for ", typeof(params))
 
+
+struct CacheDict{PT}
+    params::PT
+    caches::Dict{UInt64, IntegratorCache}
+
+    function CacheDict(params::Parameters)
+        new{typeof(params)}(params, Dict{UInt64, IntegratorCache}())
+    end
+end
+
+@inline function Base.getindex(c::CacheDict, ST::DataType)
+    key = hash(Threads.threadid(), hash(ST))
+    if haskey(c.caches, key)
+        c.caches[key]
+    else
+        c.caches[key] = IntegratorCache{ST}(c.params)
+    end::CacheType(ST, c.params)
+end
