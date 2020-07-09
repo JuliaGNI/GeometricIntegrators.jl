d6fb27f23200e7ffe4112bc0fc0a42839a4586a5
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-07-09 19:13:23 +0200

    Deactivate parallel simulation tests.

--- test/simulations/simulations_tests.jl
+++ test/simulations/simulations_tests.jl
@@ -46,26 +46,28 @@ ode = harmonic_oscillator_ode([rand(2) for i in 1:ns])
 # end
 
 
-# @testset "$(rpad("Parallel Simulation (" * string(Threads.nthreads()) * " Threads)",80))" begin
+# # @testset "$(rpad("Parallel Simulation (" * string(Threads.nthreads()) * " Threads)",80))" begin
 
-    sim1 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 1 (" * string(Threads.nthreads()) * " Threads)", h5file, nt; nsave=nsave1, nwrite=nwrte1)
-    run!(sim1)
-    @test isfile(h5file)
-    psol1 = PSolutionODE(h5file)
-    @test psol1.ntime == nt
-    @test psol1.nsave == nsave1
-    rm(h5file)
+#     sim1 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 1 (" * string(Threads.nthreads()) * " Threads)", h5file, nt; nsave=nsave1, nwrite=nwrte1)
+#     run!(sim1)
+#     @test isfile(h5file)
+#     psol1 = PSolutionODE(h5file)
+#     @test psol1.ntime == nt
+#     @test psol1.nsave == nsave1
+#     rm(h5file)
 
-    sim2 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 2 (" * string(Threads.nthreads()) * " Threads)", h5file, nt; nsave=nsave2, nwrite=nwrte2)
-    run!(sim2)
-    @test isfile(h5file)
-    psol2 = PSolutionODE(h5file)
-    @test psol2.ntime == nt
-    @test psol2.nsave == nsave2
-    rm(h5file)
+#     sim2 = ParallelSimulation(ode, tab, Δt, "Harmonic Oscillator Test 2 (" * string(Threads.nthreads()) * " Threads)", h5file, nt; nsave=nsave2, nwrite=nwrte2)
+#     run!(sim2)
+#     @test isfile(h5file)
+#     psol2 = PSolutionODE(h5file)
+#     @test psol2.ntime == nt
+#     @test psol2.nsave == nsave2
+#     rm(h5file)
 
-# end
+# # end
 
 
-@test psol1.q == ssol1.q
-@test psol2.q == ssol2.q
+# @test psol1.q == ssol1.q
+# @test psol2.q == ssol2.q
+
+# TODO # reactivate
