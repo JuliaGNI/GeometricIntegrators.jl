673bca647fafa5c296c64eb6e0d245c77e758213
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-07-09 13:19:11 +0200

    Fix StateVector definition and zero() method.

--- src/Common.jl
+++ src/Common.jl
@@ -2,10 +2,10 @@ module Common
 
     export State, StateVector
 
-    State{DT <: Number} = AbstractArray{DT}
-    StateVector{DT} = AbstractVector{<:State{DT}}
+    const State{DT <: Number} = AbstractArray{DT}
+    const StateVector{DT,VT} = VT where {DT, VT <: AbstractVector{<:State{DT}}}
 
-    Base.zero(X::AT) where {AT <: StateVector} = AT[zero(x) for x in X]
+    Base.zero(X::ST) where {DT, VT, ST <: StateVector{DT,VT}} = VT[zero(x) for x in X]
 
 
     export nbasis, nnodes, nodes, order, degree
