182a6e473b40bc2da0f456ee8e3b98cb7b20d023
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-07-10 16:04:37 +0200

    Fix and cleanup in atomic solutions.

--- src/solutions/atomic_solution.jl
+++ src/solutions/atomic_solution.jl
@@ -2,7 +2,7 @@
 """
 Abstract atomistic or single-step solution.
 """
-abstract type AtomicSolution{dType <: Number, tType <: Real} end
+abstract type AtomicSolution{dType <: Number, tType <: Real, aType <: AbstractArray{dType}} end
 
 
 "Copy solution from atomistic solution to solution object."
@@ -10,8 +10,8 @@ function copy_solution!(sol::Solution, asol::AtomicSolution, n, m)
     copy_solution!(sol, get_solution(asol)..., n, m)
 end
 
-function Common.cut_periodic_solution!(asol::AtomicSolution{DT}, periodicity::Vector{DT}) where {DT}
-    @assert length(asol.q) == length(periodicity)
+function Common.cut_periodic_solution!(asol::AtomicSolution{DT,TT,AT}, periodicity::AT) where {DT,TT,AT}
+    @assert axes(asol.q) == axes(periodicity)
     for k in eachindex(asol.q, asol.q̅, periodicity)
         if periodicity[k] ≠ 0
             while asol.q[k] < 0
