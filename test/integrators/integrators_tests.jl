a404c121afd5d156ef6b963bf3f38fe481a233cf
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-07-10 10:58:01 +0200

    Fix splitting integrators.

--- test/integrators/integrators_tests.jl
+++ test/integrators/integrators_tests.jl
@@ -8,7 +8,7 @@ using SafeTestsets
 @safetestset "VPRK Integrators                                                                " begin include("vprk_integrators_tests.jl") end
 @safetestset "SPARK Integrators                                                               " begin include("spark_integrators_tests.jl") end
 @safetestset "Special Integrators                                                             " begin include("special_integrators_tests.jl") end
-# @safetestset "Splitting Integrators                                                           " begin include("splitting_integrators_tests.jl") end
+@safetestset "Splitting Integrators                                                           " begin include("splitting_integrators_tests.jl") end
 @safetestset "Galerkin Integrators                                                            " begin include("galerkin_integrators_tests.jl") end
 # @safetestset "Stochastic Integrators                                                          " begin include("stochastic_integrators_tests.jl") end
 # TODO # reactivate
