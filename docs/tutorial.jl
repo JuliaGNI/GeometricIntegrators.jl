3fff5dab5ee98dc18199f8f758f3bcb4d4c131bd
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-11-23 13:30:57 +0100

    Update tutorial run script and documenter workflow. (+1 squashed commit) Squashed commits: [522065e] Try to fix Documenter workflow issue.
    Update tutorial run script and documenter workflow.

--- docs/tutorial.jl
+++ docs/tutorial.jl
@@ -1,8 +1,8 @@
 using GeometricIntegrators
 using Weave
 
-tutorial_path = joinpath(dirname(pathof(GeometricIntegrators)), "../docs/tutorial", "tutorial.jmd")
-build_path = joinpath(dirname(pathof(GeometricIntegrators)), "../docs/src/tutorial")
+tutorial_path = joinpath("tutorial", "tutorial.jmd")
+build_path = joinpath("src", "tutorial")
 
 weave(tutorial_path,
   out_path=build_path,
