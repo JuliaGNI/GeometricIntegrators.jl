6e03e56c79068a31075ab2e3b2c6462e07aa42c5
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-03-04 16:06:52 +0100

    Revise SODE.

--- src/equations/sode.jl
+++ src/equations/sode.jl
@@ -19,8 +19,9 @@ v (t) = v_1 (t) + ... + v_r (t) .
 * `AT <: AbstractArray{DT}`: array type
 * `vType <: Union{Tuple,Nothing}`: type of `v`
 * `qType <: Union{Tuple,Nothing}`: type of `q`
-* `hType <: OptionalFunction`: type of `h`
-* `pType <: Union{NamedTuple,Nothing}`: parameters type
+* `invType <: OptionalNamedTuple`: invariants type
+* `parType <: OptionalNamedTuple`: parameters type
+* `perType <: OptionalArray{AT}`: periodicity type
 
 ### Fields
 
@@ -29,7 +30,8 @@ v (t) = v_1 (t) + ... + v_r (t) .
 * `q`: tuple of functions computing the solution
 * `t₀`: initial time
 * `q₀`: initial condition
-* `parameters`: either a `NamedTuple` containing the equations parameters or `nothing`
+* `invariants`: either a `NamedTuple` containing the equation's invariants or `nothing`
+* `parameters`: either a `NamedTuple` containing the equation's parameters or `nothing`
 * `periodicity`: determines the periodicity of the state vector `q` for cutting periodic solutions
 
 The functions `v_i` providing the vector field must have the interface
@@ -60,59 +62,80 @@ methods, e.g., it allows to use another integrator for solving substeps.
 ### Constructors
 
 ```julia
-SODE(v, q, t₀, q₀; parameters=nothing, periodicity=zero(q₀[begin]))
+SODE(v, q, t₀, q₀, invariants, parameters, periodicity)
 
-SODE(v, q::Union{Tuple,Nothing}, t₀::Real, q₀::State; kwargs...) = SODE(v, q, t₀, [q₀]; kwargs...)
-SODE(v, q::Union{Tuple,Nothing}, q₀::StateVector; kwargs...) = SODE(v, q, 0.0, q₀; kwargs...)
-SODE(v, q::Union{Tuple,Nothing}, q₀::State; kwargs...) = SODE(v, q, 0.0, q₀; kwargs...)
+SODE(v, q::Union{Tuple,Nothing}, t₀::Real, q₀::StateVector; kwargs...)
+SODE(v, q::Union{Tuple,Nothing}, t₀::Real, q₀::State; kwargs...)
+SODE(v, q::Union{Tuple,Nothing}, q₀::StateVector; kwargs...)
+SODE(v, q::Union{Tuple,Nothing}, q₀::State; kwargs...)
 
-SODE(v, t₀::Real, q₀::StateVector; kwargs...) = SODE(v, nothing, t₀, q₀; kwargs...)
-SODE(v, t₀::Real, q₀::State; kwargs...) = SODE(v, nothing, t₀, [q₀]; kwargs...)
-SODE(v, q₀::StateVector; kwargs...) = SODE(v, nothing, 0.0, q₀; kwargs...)
-SODE(v, q₀::State; kwargs...) = SODE(v, nothing, 0.0, q₀; kwargs...)
+SODE(v, t₀::Real, q₀::StateVector; kwargs...)
+SODE(v, t₀::Real, q₀::State; kwargs...)
+SODE(v, q₀::StateVector; kwargs...)
+SODE(v, q₀::State; kwargs...)
 ```
+
+### Keyword arguments:
+
+* `invariants = nothing`
+* `parameters = nothing`
+* `periodicity = nothing`
 
 """
 struct SODE{dType <: Number, tType <: Real, arrayType <: AbstractArray{dType},
             vType <: Union{Tuple,Nothing}, qType <: Union{Tuple,Nothing},
-            pType <: Union{NamedTuple,Nothing}} <: AbstractEquationODE{dType, tType}
+            invType <: OptionalNamedTuple,
+            parType <: OptionalNamedTuple,
+            perType <: OptionalArray{arrayType}} <: AbstractEquationODE{dType, tType}
 
     d::Int
+
     v::vType
     q::qType
+
     t₀::tType
     q₀::Vector{arrayType}
-    parameters::pType
-    periodicity::arrayType
 
-    function SODE(v::vType, q::qType, t₀::tType, q₀::Vector{arrayType};
-                 parameters::pType=nothing, periodicity=zero(q₀[begin])) where {
+    invariants::invType
+    parameters::parType
+    periodicity::perType
+
+    function SODE(v::vType, q::qType, t₀::tType, q₀::Vector{arrayType},
+                 invariants::invType, parameters::parType, periodicity::perType) where {
                         dType <: Number, tType <: Real, arrayType <: AbstractArray{dType},
                         vType <: Union{Tuple,Nothing}, qType <: Union{Tuple,Nothing},
-                        pType <: Union{NamedTuple,Nothing}}
+                        invType <: OptionalNamedTuple,
+                        parType <: OptionalNamedTuple,
+                        perType <: OptionalArray{arrayType}}
 
         d = length(q₀[begin])
 
         @assert all(length(q) == d for q in q₀)
-        @assert v == nothing || all(typeof(V) <: OptionalFunction for V in v)
-        @assert q == nothing || all(typeof(Q) <: OptionalFunction for Q in q)
+        @assert v === nothing || all(typeof(V) <: OptionalFunction for V in v)
+        @assert q === nothing || all(typeof(Q) <: OptionalFunction for Q in q)
 
-        new{dType, tType, arrayType, vType, qType, pType}(d, v, q, t₀, q₀, parameters, periodicity)
+        new{dType, tType, arrayType, vType, qType, invType, parType, perType}(d, v, q, t₀, q₀, invariants, parameters, periodicity)
     end
 end
 
-SODE(v, q::Union{Tuple,Nothing}, t₀::Real, q₀::State; kwargs...) = SODE(v, q, t₀, [q₀]; kwargs...)
+_SODE(v, q::Union{Tuple,Nothing}, t₀::Real, q₀::StateVector; invariants=nothing, parameters=nothing, periodicity=nothing) = SODE(v, q, t₀, q₀, invariants, parameters, periodicity)
+
+SODE(v, q::Union{Tuple,Nothing}, t₀::Real, q₀::StateVector; kwargs...) = _SODE(v, q, t₀, q₀; kwargs...)
 SODE(v, q::Union{Tuple,Nothing}, q₀::StateVector; kwargs...) = SODE(v, q, 0.0, q₀; kwargs...)
+SODE(v, q::Union{Tuple,Nothing}, t₀::Real, q₀::State; kwargs...) = SODE(v, q, t₀, [q₀]; kwargs...)
 SODE(v, q::Union{Tuple,Nothing}, q₀::State; kwargs...) = SODE(v, q, 0.0, q₀; kwargs...)
 
-SODE(v, t₀::Real, q₀::StateVector; kwargs...) = SODE(v, nothing, t₀, q₀; kwargs...)
-SODE(v, t₀::Real, q₀::State; kwargs...) = SODE(v, nothing, t₀, [q₀]; kwargs...)
+SODE(v, t₀::Real, q₀::StateVector; kwargs...) = _SODE(v, nothing, t₀, q₀; kwargs...)
 SODE(v, q₀::StateVector; kwargs...) = SODE(v, nothing, 0.0, q₀; kwargs...)
+SODE(v, t₀::Real, q₀::State; kwargs...) = SODE(v, nothing, t₀, [q₀]; kwargs...)
 SODE(v, q₀::State; kwargs...) = SODE(v, nothing, 0.0, q₀; kwargs...)
 
-const SODEPT{PT,DT,TT,AT,VT,QT} = SODE{DT,TT,AT,VT,QT,PT} # type alias for dispatch on parameters type parameter
-const SODEQT{QT,DT,TT,AT,VT,PT} = SODE{DT,TT,AT,VT,QT,PT} # type alias for dispatch on solution type parameter
-const SODEVT{VT,DT,TT,AT,QT,PT} = SODE{DT,TT,AT,VT,QT,PT} # type alias for dispatch on vector field type parameter
+const SODEinvType{invT,DT,TT,AT,VT,QT,parT,perT} = SODE{DT,TT,AT,VT,QT,invT,parT,perT} # type alias for dispatch on invariants type parameter
+const SODEparType{parT,DT,TT,AT,VT,QT,invT,perT} = SODE{DT,TT,AT,VT,QT,invT,parT,perT} # type alias for dispatch on parameters type parameter
+const SODEperType{perT,DT,TT,AT,VT,QT,invT,parT} = SODE{DT,TT,AT,VT,QT,invT,parT,perT} # type alias for dispatch on periodicity type parameter
+
+const SODEQT{QT,DT,TT,AT,VT,invT,parT,perT} = SODE{DT,TT,AT,VT,QT,invT,parT,perT} # type alias for dispatch on solution type parameter
+const SODEVT{VT,DT,TT,AT,QT,invT,parT,perT} = SODE{DT,TT,AT,VT,QT,invT,parT,perT} # type alias for dispatch on vector field type parameter
 
 Base.hash(ode::SODE, h::UInt) = hash(ode.d, hash(ode.v, hash(ode.t₀,
         hash(ode.q₀, hash(ode.periodicity, hash(ode.parameters, h))))))
@@ -123,26 +146,17 @@ Base.:(==)(ode1::SODE, ode2::SODE) = (
                              && ode1.q == ode2.q
                              && ode1.t₀ == ode2.t₀
                              && ode1.q₀ == ode2.q₀
-                             && ode1.parameters == ode2.parameters
+                             && ode1.invariants  == ode2.invariants
+                             && ode1.parameters  == ode2.parameters
                              && ode1.periodicity == ode2.periodicity)
 
-function Base.similar(equ::SODE, t₀::Real, q₀::StateVector;
-                      parameters=equ.parameters, periodicity=equ.periodicity)
+function Base.similar(equ::SODE, t₀::Real, q₀::StateVector; parameters=equ.parameters)
     @assert all([length(q) == ndims(equ) for q in q₀])
-    SODE(equ.v, equ.q, t₀, q₀; parameters=parameters, periodicity=periodicity)
+    SODE(equ.v, equ.q, t₀, q₀; invariants=equ.invariants, parameters=parameters, periodicity=equ.periodicity)
 end
 
 Base.similar(equ::SODE, q₀; kwargs...) = similar(equ, equ.t₀, q₀; kwargs...)
 Base.similar(equ::SODE, t₀::Real, q₀::State; kwargs...) = similar(equ, t₀, [q₀]; kwargs...)
-
-Base.ndims(equ::SODE) = equ.d
-Base.axes(equ::SODE) = axes(equ.q₀[begin])
-Common.nsamples(equ::SODE) = length(equ.q₀)
-Common.periodicity(equ::SODE) = equ.periodicity
-initial_conditions(equ::SODE) = (equ.t₀, equ.q₀)
-
-hasparameters(::SODEPT{<:Nothing}) = false
-hasparameters(::SODEPT{<:NamedTuple}) = true
 
 hassolution(::SODEQT{<:Nothing}) = false
 hassolution(::SODEQT{<:Tuple}) = true # && all(typeof(Q) <: Functiong for Q in equ.q)
@@ -155,6 +169,22 @@ hasvectorfield(::SODEVT{<:Tuple}) = true # && all(typeof(V) <: Function for V in
 
 hasvectorfield(::SODEVT{<:Nothing}, i) = false
 hasvectorfield(equ::SODEVT{<:Tuple}, i) = i ≤ length(equ.v) && typeof(equ.v[i]) <: Function
+
+hasinvariants(::SODEinvType{<:Nothing}) = false
+hasinvariants(::SODEinvType{<:NamedTuple}) = true
+
+hasparameters(::SODEparType{<:Nothing}) = false
+hasparameters(::SODEparType{<:NamedTuple}) = true
+
+hasperiodicity(::SODEperType{<:Nothing}) = false
+hasperiodicity(::SODEperType{<:AbstractArray}) = true
+
+Base.axes(equ::SODE) = axes(equ.q₀[begin])
+Base.ndims(equ::SODE) = equ.d
+Common.nsamples(equ::SODE) = length(equ.q₀)
+
+@inline Common.periodicity(equation::SODE) = hasperiodicity(equation) ? equation.periodicity : zero(equation.q₀[begin])
+@inline initial_conditions(equation::SODE) = (equation.t₀, equation.q₀)
 
 _get_v(equ::SODE) = hasparameters(equ) ? Tuple(typeof(V) <: Function ? (t,q,v) -> V(t, q, v, equ.parameters) : V for V in equ.v) : equ.v
 _get_q(equ::SODE) = hasparameters(equ) ? Tuple(typeof(Q) <: Function ? (t,q̄,q,h) -> Q(t, q̄, q, h, equ.parameters) : Q for Q in equ.q) : equ.q
