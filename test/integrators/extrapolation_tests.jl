b575582b35fab8a26c5fa3a5b06dc8b4aa305f2c
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-05-18 14:53:56 +0200

    Add tests for midpoint extrapolation for IODEs.

--- test/integrators/extrapolation_tests.jl
+++ test/integrators/extrapolation_tests.jl
@@ -2,17 +2,19 @@
 using GeometricIntegrators.Common
 using GeometricIntegrators.Config
 using GeometricIntegrators.Integrators
+using GeometricIntegrators.Integrators.VPRK
 using GeometricIntegrators.Tableaus
 using GeometricProblems.HarmonicOscillator
 using Test
 
 using GeometricIntegrators.Equations: _get_v, _get_f, _get_v̄, _get_f̄
-using GeometricIntegrators.Integrators: _euler_extrapolation_ode!, _midpoint_extrapolation_ode!,
-                                        _midpoint_extrapolation_iode!, _midpoint_extrapolation_pode!
+using GeometricIntegrators.Integrators: _euler_extrapolation_ode!, _hermite_extrapolation!,
+                                        _midpoint_extrapolation_ode!, _midpoint_extrapolation_iode!, _midpoint_extrapolation_pode!
 
 const Δt = 0.1
 const t₀ = 0.0
 const t₁ = t₀ + Δt
+const t₂ = t₁ + Δt
 
 ode  = harmonic_oscillator_ode()
 pode = harmonic_oscillator_pode()
@@ -22,10 +24,14 @@ iode = harmonic_oscillator_iode()
 # Create ODE Solution Arrays
 
 x₀ = ode.q₀[1]
+x₁ = zero(x₀)
+x₂ = zero(x₀)
 xᵢ = zero(x₀)
 
 ẋₚ = zero(x₀)
 ẋ₀ = zero(x₀)
+ẋ₁ = zero(x₀)
+ẋ₂ = zero(x₀)
 ẋₙ = zero(x₀)
 ẋᵢ = zero(x₀)
 
@@ -41,55 +47,16 @@ xₚ = ref_prev.q[end]
 tₙ = ref_next.t[end]
 xₙ = ref_next.q[end]
 
+tᵢ = tₙ
+
 ode.v(tₚ, xₚ, ẋₚ, ode.parameters)
 ode.v(t₀, x₀, ẋ₀, ode.parameters)
 ode.v(tₙ, xₙ, ẋₙ, ode.parameters)
 
 
-# Create PODE Solution Arrays
-
-q₀ = pode.q₀[1]
-p₀ = pode.p₀[1]
-
-qᵢ = zero(q₀)
-pᵢ = zero(p₀)
-
-q̇ₚ = zero(q₀)
-q̇₀ = zero(q₀)
-q̇ₙ = zero(q₀)
-q̇ᵢ = zero(q₀)
-
-ṗₚ = zero(p₀)
-ṗ₀ = zero(p₀)
-ṗₙ = zero(p₀)
-ṗᵢ = zero(p₀)
-
-
-# Compute Reference Solution for PODEs
-
-ref_prev = integrate(pode, PartitionedTableauGauss(8), -Δt, 1)
-ref_next = integrate(pode, PartitionedTableauGauss(8), +Δt, 1)
-
-qₚ = ref_prev.q[end]
-pₚ = ref_prev.p[end]
-
-qₙ = ref_next.q[end]
-pₙ = ref_next.p[end]
-
-pode.v(tₚ, qₚ, pₚ, q̇ₚ, ode.parameters)
-pode.v(t₀, q₀, p₀, q̇₀, ode.parameters)
-pode.v(tₙ, qₙ, pₙ, q̇ₙ, ode.parameters)
-
-pode.f(tₚ, qₚ, pₚ, ṗₚ, ode.parameters)
-pode.f(t₀, q₀, p₀, ṗ₀, ode.parameters)
-pode.f(tₙ, qₙ, pₙ, ṗₙ, ode.parameters)
-
-
-
 # Hermite Extrapolation
 
-int = HermiteExtrapolation(zero(Δt), one(Δt), Δt)
-evaluate!(int, xₚ, x₀, ẋₚ, ẋ₀, 2one(Δt), xᵢ, ẋᵢ)
+evaluate!(HermiteExtrapolation(tₚ, t₀), xₚ, x₀, ẋₚ, ẋ₀, tᵢ, xᵢ, ẋᵢ)
 
 # println(xᵢ, xₙ, xᵢ .- xₙ)
 # println(ẋᵢ, ẋₙ, ẋᵢ .- ẋₙ)
@@ -98,7 +65,8 @@ evaluate!(int, xₚ, x₀, ẋₚ, ẋ₀, 2one(Δt), xᵢ, ẋᵢ)
 @test ẋᵢ ≈ ẋₙ atol=1E-4
 
 
-# Euler Extrapolation
+
+# Euler Extrapolation for ODEs
 
 _euler_extrapolation_ode!(_get_v(ode), t₀, t₁, x₀, xᵢ, 0)
 # println(xᵢ, xₙ, xᵢ .- xₙ)
@@ -168,24 +136,63 @@ for i in 0:5
 end
 
 
+# Create PODE Solution Arrays
+
+q₀ = pode.q₀[1]
+p₀ = pode.p₀[1]
+
+qᵢ = zero(q₀)
+pᵢ = zero(p₀)
+
+q̇ₚ = zero(q₀)
+q̇₀ = zero(q₀)
+q̇ₙ = zero(q₀)
+q̇ᵢ = zero(q₀)
+
+ṗₚ = zero(p₀)
+ṗ₀ = zero(p₀)
+ṗₙ = zero(p₀)
+ṗᵢ = zero(p₀)
+
+
+# Compute Reference Solution for PODEs
+
+ref_prev = integrate(pode, PartitionedTableauGauss(8), -Δt, 1)
+ref_next = integrate(pode, PartitionedTableauGauss(8), +Δt, 1)
+
+qₚ = ref_prev.q[end]
+pₚ = ref_prev.p[end]
+
+qₙ = ref_next.q[end]
+pₙ = ref_next.p[end]
+
+pode.v(tₚ, qₚ, pₚ, q̇ₚ, pode.parameters)
+pode.v(t₀, q₀, p₀, q̇₀, pode.parameters)
+pode.v(tₙ, qₙ, pₙ, q̇ₙ, pode.parameters)
+
+pode.f(tₚ, qₚ, pₚ, ṗₚ, pode.parameters)
+pode.f(t₀, q₀, p₀, ṗ₀, pode.parameters)
+pode.f(tₙ, qₙ, pₙ, ṗₙ, pode.parameters)
+
+
 # Midpoint Extrapolation for PODEs
 
 _midpoint_extrapolation_pode!(_get_v(pode), _get_f(pode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 0)
 # println(0, qᵢ, qₙ, qᵢ .- qₙ)
 # println(0, pᵢ, pₙ, pᵢ .- pₙ)
-@test qᵢ ≈ qₙ atol=1E-4
+@test qᵢ ≈ qₙ atol=1E-6
 @test pᵢ ≈ pₙ atol=1E-4
 
 _midpoint_extrapolation_pode!(_get_v(pode), _get_f(pode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 1)
 # println(1, qᵢ, qₙ, qᵢ .- qₙ)
 # println(1, pᵢ, pₙ, pᵢ .- pₙ)
-@test qᵢ ≈ qₙ atol=1E-8
+@test qᵢ ≈ qₙ atol=1E-10
 @test pᵢ ≈ pₙ atol=1E-8
 
 _midpoint_extrapolation_pode!(_get_v(pode), _get_f(pode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 2)
 # println(2, qᵢ, qₙ, qᵢ .- qₙ)
 # println(2, pᵢ, pₙ, pᵢ .- pₙ)
-@test qᵢ ≈ qₙ atol=1E-12
+@test qᵢ ≈ qₙ atol=1E-14
 @test pᵢ ≈ pₙ atol=1E-12
 
 _midpoint_extrapolation_pode!(_get_v(pode), _get_f(pode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 3)
@@ -204,7 +211,7 @@ _midpoint_extrapolation_pode!(_get_v(pode), _get_f(pode), t₀, t₁, q₀, qᵢ
 # println(5, qᵢ, qₙ, qᵢ .- qₙ)
 # println(5, pᵢ, pₙ, pᵢ .- pₙ)
 @test qᵢ ≈ qₙ atol=1E-15
-@test pᵢ ≈ pₙ atol=1E-15
+@test pᵢ ≈ pₙ atol=1E-16
 
 for i in 0:5
     extrap1 = MidpointExtrapolation(pode, i)
@@ -212,4 +219,91 @@ for i in 0:5
     @test extrap1 == extrap2
     @test evaluate!(extrap1, t₀, t₁, q₀, qᵢ, p₀, pᵢ) == _midpoint_extrapolation_pode!(_get_v(pode), _get_f(pode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, i)
     @test evaluate!(extrap2, t₀, t₁, q₀, qᵢ, p₀, pᵢ) == _midpoint_extrapolation_pode!(_get_v(pode), _get_f(pode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, i)
+end
+
+
+# Create IODE Solution Arrays
+
+q₀ = iode.q₀[1]
+p₀ = iode.p₀[1]
+
+qᵢ = zero(q₀)
+pᵢ = zero(p₀)
+
+q̇ₚ = zero(q₀)
+q̇₀ = zero(q₀)
+q̇ₙ = zero(q₀)
+q̇ᵢ = zero(q₀)
+
+ṗₚ = zero(p₀)
+ṗ₀ = zero(p₀)
+ṗₙ = zero(p₀)
+ṗᵢ = zero(p₀)
+
+
+# Compute Reference Solution for IODEs
+
+ref_prev = integrate(iode, IntegratorVPRKpSymmetric(iode, TableauVPGLRK(8), -Δt), 1)
+ref_next = integrate(iode, IntegratorVPRKpSymmetric(iode, TableauVPGLRK(8), +Δt), 1)
+
+qₚ = ref_prev.q[end]
+pₚ = ref_prev.p[end]
+
+qₙ = ref_next.q[end]
+pₙ = ref_next.p[end]
+
+iode.v̄(tₚ, qₚ, q̇ₚ, iode.parameters)
+iode.v̄(t₀, q₀, q̇₀, iode.parameters)
+iode.v̄(tₙ, qₙ, q̇ₙ, iode.parameters)
+
+iode.f̄(tₚ, qₚ, q̇ₚ, ṗₚ, iode.parameters)
+iode.f̄(t₀, q₀, q̇₀, ṗ₀, iode.parameters)
+iode.f̄(tₙ, qₙ, q̇ₙ, ṗₙ, iode.parameters)
+
+
+
+# Midpoint Extrapolation for IODEs
+
+_midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 0)
+# println(0, qᵢ, qₙ, qᵢ .- qₙ)
+# println(0, pᵢ, pₙ, pᵢ .- pₙ)
+@test qᵢ ≈ qₙ atol=1E-4
+@test pᵢ ≈ pₙ atol=1E-4
+
+_midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 1)
+# println(1, qᵢ, qₙ, qᵢ .- qₙ)
+# println(1, pᵢ, pₙ, pᵢ .- pₙ)
+@test qᵢ ≈ qₙ atol=1E-8
+@test pᵢ ≈ pₙ atol=1E-8
+
+_midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 2)
+# println(2, qᵢ, qₙ, qᵢ .- qₙ)
+# println(2, pᵢ, pₙ, pᵢ .- pₙ)
+@test qᵢ ≈ qₙ atol=1E-12
+@test pᵢ ≈ pₙ atol=1E-12
+
+_midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 3)
+# println(3, qᵢ, qₙ, qᵢ .- qₙ)
+# println(3, pᵢ, pₙ, pᵢ .- pₙ)
+@test qᵢ ≈ qₙ atol=1E-16
+@test pᵢ ≈ pₙ atol=1E-16
+
+_midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 4)
+# println(4, qᵢ, qₙ, qᵢ .- qₙ)
+# println(4, pᵢ, pₙ, pᵢ .- pₙ)
+@test qᵢ ≈ qₙ atol=1E-16
+@test pᵢ ≈ pₙ atol=1E-16
+
+_midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, 5)
+# println(5, qᵢ, qₙ, qᵢ .- qₙ)
+# println(5, pᵢ, pₙ, pᵢ .- pₙ)
+@test qᵢ ≈ qₙ atol=1E-15
+@test pᵢ ≈ pₙ atol=1E-16
+
+for i in 0:5
+    extrap1 = MidpointExtrapolation(iode, i)
+    extrap2 = MidpointExtrapolationIODE(_get_v̄(iode), _get_f̄(iode), i)
+    @test extrap1 == extrap2
+    @test evaluate!(extrap1, t₀, t₁, q₀, qᵢ, p₀, pᵢ) == _midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, i)
+    @test evaluate!(extrap2, t₀, t₁, q₀, qᵢ, p₀, pᵢ) == _midpoint_extrapolation_iode!(_get_v̄(iode), _get_f̄(iode), t₀, t₁, q₀, qᵢ, p₀, pᵢ, i)
 end
