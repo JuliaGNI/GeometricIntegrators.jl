c1548e40adcc80a14ddff05174c9d384d71b421f
Michael Kraus <michael.kraus@ipp.mpg.de>
2022-11-14 18:01:46 +0100

    Remove obsolete build files.

--- docs/travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-    cd docs
-    julia --project=. --color=yes -e 'using Pkg; Pkg.instantiate();';
-    julia --project=. --color=yes docs/tutorial.jl
-    julia --project=. --color=yes docs/make.jl
-fi
