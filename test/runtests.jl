6a071c575412b5a05e4ec9410da9bdfac17fe51b
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-05-17 18:13:10 +0200

    Reorder tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,13 +6,13 @@ using SafeTestsets
 @safetestset "Initial Guesses                                                                 " begin include("integrators/initial_guess_tests.jl") end
 @safetestset "Runge-Kutta Integrators                                                         " begin include("integrators/rk_integrators_tests.jl") end
 @safetestset "Runge-Kutta Integrators for Implicit Equations                                  " begin include("integrators/rk_implicit_integrators_tests.jl") end
+@safetestset "Splitting Integrators                                                           " begin include("integrators/splitting_integrators_tests.jl") end
 @safetestset "Variational Integrators                                                         " begin include("integrators/variational_integrators_tests.jl") end
 @safetestset "Degenerate Variational Integrators                                              " begin include("integrators/dvi_integrators_tests.jl") end
 
 # @safetestset "Galerkin Variational Integrators                                                " begin include("integrators/galerkin_integrators_tests.jl") end
-@safetestset "SPARK Integrators                                                               " begin include("integrators/spark_integrators_tests.jl") end
-@safetestset "Splitting Integrators                                                           " begin include("integrators/splitting_integrators_tests.jl") end
 
+@safetestset "SPARK Integrators                                                               " begin include("integrators/spark_integrators_tests.jl") end
 @safetestset "Projection Methods                                                              " begin include("projections/projections_tests.jl") end
 @safetestset "Projection Methods with Implicit Equations                                      " begin include("projections/projections_implicit_tests.jl") end
 @safetestset "Projection Methods with Variational Partitioned Runge-Kutta Integrators         " begin include("projections/projections_vprk_tests.jl") end
