591fdfcd168a081fc1ccec8d1f073906c24e2710
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-08-29 12:35:22 +0900

    Reorder tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,9 +12,6 @@ using SafeTestsets
 
 # @safetestset "Galerkin Variational Integrators                                                " begin include("integrators/galerkin_integrators_tests.jl") end
 
-@safetestset "SPARK Integrators                                                               " begin include("spark/spark_integrators_tests.jl") end
-@safetestset "SPARK Tableau Tests                                                             " begin include("spark/spark_tableaus_tests.jl") end
-
 @safetestset "Projection Methods                                                              " begin include("projections/projections_tests.jl") end
 @safetestset "Projection Methods with Implicit Equations                                      " begin include("projections/projections_implicit_tests.jl") end
 @safetestset "Projection Methods with Variational Partitioned Runge-Kutta Integrators         " begin include("projections/projections_vprk_tests.jl") end
@@ -22,5 +19,8 @@ using SafeTestsets
 @safetestset "Ensemble Integrator Tests                                                       " begin include("integrators/ensemble_integrators_tests.jl") end
 
 @safetestset "Method Tests                                                                    " begin include("methods/methods_tests.jl") end
+
+@safetestset "SPARK Integrators                                                               " begin include("spark/spark_integrators_tests.jl") end
+@safetestset "SPARK Tableau Tests                                                             " begin include("spark/spark_tableaus_tests.jl") end
 
 # @safetestset "Simulation Tests                                                                " begin include("simulations/simulations_tests.jl") end
