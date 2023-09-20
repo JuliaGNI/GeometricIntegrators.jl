48a9e84dc29f1d3d1ba341cfffbe1e9850dda401
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-09-20 20:06:37 +0200

    Reorganise tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,12 @@ using SafeTestsets
 @safetestset "Extrapolation Methods                                                           " begin include("extrapolation_tests.jl") end
 @safetestset "Solution Tests                                                                  " begin include("solutions/solutions_tests.jl") end
 @safetestset "Initial Guesses                                                                 " begin include("integrators/initial_guess_tests.jl") end
+
 @safetestset "Euler Integrators                                                               " begin include("integrators/euler_tests.jl") end
 @safetestset "Runge-Kutta Integrators                                                         " begin include("integrators/rk_integrators_tests.jl") end
 @safetestset "Runge-Kutta Integrators for Implicit Equations                                  " begin include("integrators/rk_implicit_integrators_tests.jl") end
 @safetestset "Splitting Integrators                                                           " begin include("integrators/splitting_integrators_tests.jl") end
+
 @safetestset "Variational Integrators                                                         " begin include("integrators/variational_integrators_tests.jl") end
 @safetestset "Degenerate Variational Integrators                                              " begin include("integrators/dvi_integrators_tests.jl") end
 @safetestset "Galerkin Variational Integrators                                                " begin include("integrators/galerkin_integrators_tests.jl") end
@@ -19,8 +21,7 @@ using SafeTestsets
 @safetestset "Ensemble Integrator Tests                                                       " begin include("integrators/ensemble_integrators_tests.jl") end
 
 @safetestset "Method Tests                                                                    " begin include("methods/methods_tests.jl") end
+@safetestset "Simulation Tests                                                                " begin include("simulations/simulations_tests.jl") end
 
 @safetestset "SPARK Integrators                                                               " begin include("spark/spark_integrators_tests.jl") end
 @safetestset "SPARK Tableau Tests                                                             " begin include("spark/spark_tableaus_tests.jl") end
-
-@safetestset "Simulation Tests                                                                " begin include("simulations/simulations_tests.jl") end
