cc4e99e2ed5dc2b02b785679bc7e3d914bab8f23
Michael Kraus <michael.kraus@ipp.mpg.de>
2026-07-24 11:19:15 +0200

    Remove more obsolete comments.

--- test/integrators/variational_integrators_tests.jl
+++ test/integrators/variational_integrators_tests.jl
@@ -12,7 +12,6 @@ pref = exact_solution(podeproblem())
 @testset "$(rpad("Discrete Euler-Lagrange integrators",80))" begin
 
     sol = integrate(deleproblem_midpoint(), DiscreteEulerLagrange())
-    # println(relative_maximum_error(sol.q, pref.q))
     @test relative_maximum_error(sol.q, pref.q) < 4E-4
 
     dele = DELEProblem(lode, Midpoint())
@@ -21,7 +20,6 @@ pref = exact_solution(podeproblem())
     @test relative_maximum_error(dsol.q, sol.q) < 1E-14
 
     sol = integrate(deleproblem_trapezoidal(), DiscreteEulerLagrange())
-    # println(relative_maximum_error(sol.q, pref.q))
     @test relative_maximum_error(sol.q, pref.q) < 4E-4
 
     dele = DELEProblem(lode, Trapezoidal())
@@ -45,8 +43,6 @@ end
 @testset "$(rpad("Vartiational Partitioned Runge-Kutta integrators",80))" begin
 
     sol = integrate(lode, VPRKGauss(1))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 4E-4
     @test relative_maximum_error(sol.p, pref.p) < 8E-4
 
@@ -54,28 +50,19 @@ end
     @test relative_maximum_error(sol.q, ref.q) < 8 * eps()
 
     sol = integrate(lode, VPRKGauss(2))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 4E-8
     @test relative_maximum_error(sol.p, pref.p) < 8E-8
 
     sol = integrate(lode, VPRKGauss(3))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 8E-13
     @test relative_maximum_error(sol.p, pref.p) < 4E-12
 
     sol = integrate(lode, VPRKGauss(4))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 4E-16
-    # q measured 2.63E-16 (~eps): kept at roundoff floor 4E-16; sol.q != pref.q exactly, so 2E-16 not reachable
     @test relative_maximum_error(sol.p, pref.p) < 1E-15
 
 
     sol = integrate(lode, VPRKLobattoIIIAIIIĀ(2))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 2E-4
     @test relative_maximum_error(sol.p, pref.p) < 1E-3
 
@@ -83,33 +70,23 @@ end
     @test relative_maximum_error(sol.q, ref.q) < 8 * eps()
 
     sol = integrate(lode, VPRKLobattoIIIAIIIĀ(3))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 8E-9
     @test relative_maximum_error(sol.p, pref.p) < 2E-7
 
     sol = integrate(lode, VPRKLobattoIIIAIIIĀ(4))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 2E-13
     @test relative_maximum_error(sol.p, pref.p) < 4E-12
 
 
     sol = integrate(lode, VPRKLobattoIIIBIIIB̄(2))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 4E-4
     @test relative_maximum_error(sol.p, pref.p) < 8E-4
 
     sol = integrate(lode, VPRKLobattoIIIBIIIB̄(3))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 4E-8
     @test relative_maximum_error(sol.p, pref.p) < 8E-8
 
     sol = integrate(lode, VPRKLobattoIIIBIIIB̄(4))
-    # println(relative_maximum_error(sol.q, pref.q))
-    # println()
     @test relative_maximum_error(sol.q, pref.q) < 8E-13
     @test relative_maximum_error(sol.p, pref.p) < 4E-12
 
